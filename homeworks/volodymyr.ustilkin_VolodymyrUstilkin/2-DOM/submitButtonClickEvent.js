4c9bbce978677aa4c77626e0a5dfacee26658865
Volodymyr Ustilkin <volodymyr.ustilkin@gmail.com>
2020-10-28 19:29:06 +0200

    hw2 fixed

---
+++ homeworks/volodymyr.ustilkin_VolodymyrUstilkin/2-DOM/submitButtonClickEvent.js
@@ -0,0 +1,32 @@
+function cleanText(text) {
+    return text.trim();
+}
+
+function textCorrectCheck(text) {
+    return text.length > 0;
+}
+
+function addText(text) {
+    const showMessagesContainer = document.getElementById('show-messages-container');
+    const textContainer = document.createElement('div');
+
+    textContainer.setAttribute('class', 'message-container');
+    textContainer.innerHTML = text;
+    showMessagesContainer.appendChild(textContainer);
+}
+
+function addNewMessage() {
+    const inputtedText = document.getElementById('inputted-text');
+    const text = inputtedText.value;
+
+    const cleanedText = cleanText(text);
+    if (textCorrectCheck(cleanedText)) {
+        addText(cleanedText);
+        inputtedText.value = '';
+    } else {
+        alert(`Text is not corrected ${cleanedText}`);
+    }
+}
+
+document.getElementById('submit-button')
+    .addEventListener('click', addNewMessage);
