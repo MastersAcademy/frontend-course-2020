4e34956c9f40f45f644b7a0a09456d5167978d53
rostyslav <ronyahavuka@gmail.com>
2020-10-28 17:57:15 +0200

    fixed with Es Lint

--- homeworks/rostyslav.havuka_RostyslavHavuka/simpleChat/main.js
+++ homeworks/rostyslav.havuka_RostyslavHavuka/simpleChat/main.js
@@ -1,5 +1,5 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const button = document.getElementById('button')
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('button');
     const msgInput = document.getElementById('input');
     const messageFlowList = document.getElementById('messages');
 
@@ -10,13 +10,12 @@ document.addEventListener("DOMContentLoaded", () => {
     function validateMessage(messege) {
         try {
             if (!messege) throw new Error('input should not be empty');
-            if (!(messege === messege.trim())) throw new Error('input must not contain spaces')
+            if (!(messege === messege.trim())) throw new Error('input must not contain spaces');
             return messege;
         } catch (error) {
             alert(error);
             throw error;
         }
-
     }
 
     function createMessage(messege) {
@@ -26,9 +25,9 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     function insertMessage(messageBubble) {
-        messageFlowList.appendChild(messageBubble)
+        messageFlowList.appendChild(messageBubble);
     }
-    
+
     function clearInput() {
         msgInput.value = '';
     }
@@ -46,7 +45,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     function clickHandler(event) {
-        if (event.type === 'mouseup') mainAppFlow()
+        if (event.type === 'mouseup') mainAppFlow();
     }
 
     button.addEventListener('mouseup', clickHandler);
