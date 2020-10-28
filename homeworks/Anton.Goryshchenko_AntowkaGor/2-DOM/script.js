3a507b3edb6ebe573e5534feeca239553bc10488
AntowkaGor <antowka_gor@ukr.net>
2020-10-28 18:42:53 +0200

    2-DOM

--- homeworks/Anton.Goryshchenko_AntowkaGor/2-DOM/script.js
+++ homeworks/Anton.Goryshchenko_AntowkaGor/2-DOM/script.js
@@ -1,25 +1,24 @@
 const massageText = document.querySelector('#massageText');
 const button = document.querySelector('#btn');
 
-
-massageText.addEventListener("keyup", ({ key }) => key === "Enter" && Submit())
-button.addEventListener("click", Submit)
-
 function Submit() {
-    const text = massageText.value
-    if (!text) return
-    massageText.value = ""
+    const text = massageText.value;
+    if (!text) return;
+    massageText.value = '';
 
-    const p = document.createElement('p')
+    const p = document.createElement('p');
     p.innerText = text.trim();
-    p.classList.add('massage')
-    document.querySelector('.massageBlok').append(p)
+    p.classList.add('massage');
+    document.querySelector('.massageBlok').append(p);
 }
 
 let timer = 0;
 massageText.oninput = function () {
-    const result = document.querySelector('.typing')
+    const result = document.querySelector('.typing');
     result.innerHTML = 'typing...';
-    clearTimeout(timer)
-    timer = setTimeout(() => result.innerHTML = '', 3000)
+    clearTimeout(timer);
+    timer = setTimeout(() => { result.innerHTML = ''; }, 3000);
 };
+
+massageText.addEventListener('keyup', ({ key }) => key === 'Enter' && Submit());
+button.addEventListener('click', Submit);
