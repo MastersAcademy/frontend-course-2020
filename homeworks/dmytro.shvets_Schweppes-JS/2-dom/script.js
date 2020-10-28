29d9e7955b714982080fe5f84c971182395d6841
Schweppes-JS <shvetsdmytro90@ukr.net>
2020-10-28 18:43:26 +0200

    mistake correction

---
+++ homeworks/dmytro.shvets_Schweppes-JS/2-dom/script.js
@@ -0,0 +1,69 @@
+const container = document.querySelector('[data-messages]');
+const form = document.querySelector('[data-form]');
+const input = document.querySelector('[data-input]');
+
+let statusTyping = false;
+let messages = [];
+let timeout;
+
+// Getting current array of messages
+function gettingMessageAray() {
+    messages = Array.from(document.querySelectorAll('[data-text]'));
+}
+
+// Creating cell in DOM
+function cereatingCell(text) {
+    const elementNode = document.createElement('p');
+    gettingMessageAray();
+    (messages.length % 2 === 0) ? elementNode.classList.add('first-user') : elementNode.classList.add('second-user');
+    elementNode.setAttribute('data-text', '');
+    // Adding an avatar
+    const userIconNode = document.createElement('i');
+    if(elementNode.classList.contains('first-user')) {
+        userIconNode.classList.add('fas', 'fa-user-astronaut');
+    } else {
+        userIconNode.classList.add('fas', 'fa-user-secret');
+    }
+    elementNode.append(userIconNode);
+    // Create element for text
+    const textNode = document.createElement('span');
+    elementNode.append(textNode);
+    // Insert element and text in DOM
+    container.append(elementNode);
+    textNode.innerHTML = text;
+}
+
+// Deliting tiping status
+function deletingStatus() {
+    gettingMessageAray();
+    messages.forEach((elem) => {
+        if(elem.lastChild.innerHTML === 'Typing . . .' ) {
+            elem.remove();
+            statusTyping = !statusTyping;
+        }
+    })
+}
+
+// Sending message 
+function addingMessage(e) {
+    e.preventDefault();
+    deletingStatus();
+    cereatingCell(input.value.trim());
+    container.scrollBy(0, 50);
+    // Clear input
+    form.reset();
+}
+
+// setting status on keydown and remove after 1 second
+function addingStatusTyping() {
+    container.scrollBy(0, 50);
+    if(!statusTyping) {
+        cereatingCell('Typing . . .');
+        statusTyping = !statusTyping;
+    }
+    clearTimeout(timeout);
+    timeout = setTimeout(deletingStatus, 1000);
+}
+
+form.addEventListener('submit', addingMessage);
+input.addEventListener('keydown', addingStatusTyping);
