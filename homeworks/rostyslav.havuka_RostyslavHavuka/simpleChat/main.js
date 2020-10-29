d4a20fc377d6a54aed6db0855af661d20dbb6b7a
rostyslav <ronyahavuka@gmail.com>
2020-10-29 23:22:55 +0200

    Add typing status

--- homeworks/rostyslav.havuka_RostyslavHavuka/simpleChat/main.js
+++ homeworks/rostyslav.havuka_RostyslavHavuka/simpleChat/main.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const button = document.getElementById('button');
     const msgInput = document.getElementById('input');
     const messageFlowList = document.getElementById('messages');
+    const typingStatus = document.getElementById('typing-status');
 
     function getMessage() {
         return msgInput.value;
@@ -41,13 +42,31 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function keyboardHandler(event) {
-        if (event.key === 'Enter') mainAppFlow();
+        if (event.key === 'Enter') {
+            mainAppFlow();
+        }
     }
 
     function clickHandler() {
         mainAppFlow();
     }
 
+    let isTyping = false;
+
+    function typing() {
+        if (isTyping) return false;
+        isTyping = true;
+        const status = document.createElement('a');
+        status.innerText = 'Typing..';
+        typingStatus.appendChild(status);
+        setTimeout(() => {
+            status.innerText = '';
+            isTyping = false;
+        }, 1000);
+        return typing;
+    }
+
     button.addEventListener('click', clickHandler);
     msgInput.addEventListener('keypress', keyboardHandler);
+    msgInput.addEventListener('keydown', typing);
 });
