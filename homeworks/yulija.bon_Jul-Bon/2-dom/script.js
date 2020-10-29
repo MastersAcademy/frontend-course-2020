5418f6b7e098650136309043ce27849affa13c7e
yuliia.bon <yuliia.bon@masterofcode.com>
2020-10-29 23:02:58 +0200

    crate trmplate for message and apend message in block, added styles

--- homeworks/yulija.bon_Jul-Bon/2-dom/script.js
+++ homeworks/yulija.bon_Jul-Bon/2-dom/script.js
@@ -1,13 +1,26 @@
 const input = document.querySelector('[data-text]');
 const button = document.querySelector('[data-send]');
-const message = document.querySelector('[data-message]');
+const container = document.querySelector('#messages-box');
+const messageTemplate = document.querySelector('#message-template');
 
 button.onclick = function () {
     const inputText = input.value;
+    const inputTextReplace = inputText.replace(/^\s*/, '').replace(/\s*$/, '');
+    console.log(inputText);
+    console.log(inputTextReplace);
+
     if (inputText !== '') {
-        message.innerHTML = inputText;
+        const cloneTemplate = messageTemplate.content.cloneNode(true);
+        const messageBody = cloneTemplate.querySelectorAll('[data-message]');
+        messageBody[0].textContent = inputTextReplace;
+        container.appendChild(cloneTemplate);
         input.value = '';
     } else {
         console.log('error');
     }
+
+    // const scrollinDiv = document.querySelector('.messages-box ');
+    // setInterval(function () {
+    //     scrollinDiv.scrollTop = 9999;
+    // }, 100);
 };
