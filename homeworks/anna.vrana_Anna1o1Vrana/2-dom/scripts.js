b03dee3e8c4b7634a3f568573a67a98a10c354c9
Anna Vrana <anna.o.vrana@gmail.com>
2020-10-29 16:15:53 +0200

    create fanction to output input text

--- homeworks/anna.vrana_Anna1o1Vrana/2-dom/scripts.js
+++ homeworks/anna.vrana_Anna1o1Vrana/2-dom/scripts.js
@@ -1,40 +1,7 @@
-function onPageLoaded() {
-    const input = document.querySelector("input[type='text']");
-    const ul = document.querySelector('ul.message-list');
+function fun() {
+    const newTxt = document.querySelector("#user-text").value;
+    const vlres = document.querySelector("#valueResualt").innerHTML = newTxt;
+    const lengthNewTxt = newTxt.length;
 
-    function createToDo() {
-        const li = document.createElement('li');
-        const textSpan = document.createElement('span');
-        textSpan.classList.add('todo-text');
-        const newTodo = input.value;
-        textSpan.append(newTodo);
-        input.addEventListener('keypress', (keypassed) => {
-            const keyEnter = 13;
-            if (keyPressed.which == keyEnter) {
-                createTodo();
-            }
-        });
-        ul.addEventListener('DOMContentLoaded', onDPageLoaded);
-    }
-
-
-
-
-    //const inputNode = document.querySelector('[data-inputTxt]');
-    //messageNode = document.querySelector('[data-message-line]');
-
-    //const btnSandNode = document.querySelector('[data-send]');
-
-    //  btnSandNode.addEventListener('click', getValue);
-
-    // function getValue() {
-    //    if (!inputNode.value || inputNode.value.trim().length === 0) {
-    //         inputNode.value = '';
-    //    } else {
-    //       const copyLi = messageNode.cloneNode();
-    //       copyLi.textContent = inputNode.value.trim();
-    //      copyLi.classList.remove('hidden');
-    //       ulNode.appendChild(copyLi);
-    //       inputNode.value = '';
-    //   }
+    document.querySelector("#user-text").value = '';
 }
