f45a44fff842819ff8df3585506957e20f404486
boris <boris@example.com>
2020-10-29 10:30:17 +0200

    added new files

---
+++ homeworks/eva.pavlenko_Evapavl/2-dom/script/script.js
@@ -0,0 +1,23 @@
+let button = document.getElementById("button");
+let input = document.getElementById("input");
+
+button.addEventListener("click", () => {
+    let chatNode = document.getElementById("chat");
+    let messageNode = document.createElement("div");
+
+    if (input.value) {
+        messageNode.innerHTML = input.value;
+        chatNode.appendChild(messageNode);
+        messageNode.classList.add("message");
+    }
+    input.value = "";
+});
+
+input.addEventListener("keyup", () => {
+    const typingText = document.querySelector(".typing");
+    typingText.hidden = false;
+    setTimeout(() => {
+        typingText.hidden = true;
+    }, 1000);
+    return typingText;
+});
