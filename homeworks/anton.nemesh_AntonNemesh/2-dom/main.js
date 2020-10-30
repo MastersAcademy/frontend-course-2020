1f2463a62ad8b2c2d263628ba8a5de14f8a79740
Anton Nemesh <anton.nemesh@gmail.com>
2020-10-30 17:40:55 +0200

    chat

---
+++ homeworks/anton.nemesh_AntonNemesh/2-dom/main.js
@@ -0,0 +1,32 @@
+const form = document.querySelector('[data-form]');
+const input = document.querySelector('[data-input]');
+
+function sendMessage() {
+    const value = input.value;
+    if (validation(value)) {
+        const content = document.querySelector('[data-message-template]').content;
+        const message = content.querySelector('p');
+        message.textContent = value;
+        document.querySelector('[data-messages]').appendChild(content.cloneNode(true));
+        input.value = "";
+    }
+}
+
+function validation(value) {
+    const valueParse = value.split('');
+    if (value.length === 0) {
+        alert('Поле пустое, введите значение в поле и повторите попытку!');
+        return false;
+    }
+    if (valueParse[0] === " ") {
+        alert('Пробелы в начале строки, удалите пробелы в начале строки и повторите попытку!');
+        return false;
+    }
+    if (valueParse[valueParse.length-1] === " ") {
+        alert('Пробелы в конце строки, удалите пробелы в конце строки и повторите попытку!');
+        return false;
+    }
+    return true;
+}
+
+form.addEventListener('submit', sendMessage);
