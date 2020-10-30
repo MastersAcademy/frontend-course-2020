5560219217d0f277b1a4e87fb97a90de65722f75
Anna Vrana <anna.o.vrana@gmail.com>
2020-10-30 14:53:23 +0200

    create function'sendMessage'

--- homeworks/anna.vrana_Anna1o1Vrana/2-dom/scripts.js
+++ homeworks/anna.vrana_Anna1o1Vrana/2-dom/scripts.js
@@ -1,7 +1,21 @@
-function fun() {
-    const newTxt = document.querySelector("#user-text").value;
-    const vlres = document.querySelector("#valueResualt").innerHTML = newTxt;
-    const lengthNewTxt = newTxt.length;
+const btnSendNode = document.querySelector('#btn-send');
+const valueResualt = document.querySelector('#valueResualt');
 
-    document.querySelector("#user-text").value = '';
+function sendMessage() {
+    const newTxt = document.querySelector('#user-text').value;
+
+    if (newTxt.trim() !== '' && 'content' in document.createElement('template')) {
+        const template = document.querySelector('#template');
+        const clone = template.content.cloneNode(true);
+        const mesList = document.querySelector('#message-list');
+        const listItem = clone.querySelector('#template-item');
+        listItem.textContent = newTxt;
+        mesList.appendChild(clone);
+    } else {
+        const vlres = valueResualt.innerHTML = newTxt;
+        valueResualt.classList.remove('hide');
+    }
+    document.querySelector('#user-text').value = '';
 }
+
+btnSendNode.addEventListener('click', sendMessage);
