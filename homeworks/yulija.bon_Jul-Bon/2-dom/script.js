ba5b48b2131226cdf138b5c02458f50c8aa99f13
yuliia.bon <yuliia.bon@masterofcode.com>
2020-10-30 20:58:42 +0200

    added validation error message

--- homeworks/yulija.bon_Jul-Bon/2-dom/script.js
+++ homeworks/yulija.bon_Jul-Bon/2-dom/script.js
@@ -2,25 +2,38 @@ const input = document.querySelector('[data-text]');
 const button = document.querySelector('[data-send]');
 const container = document.querySelector('#messages-box');
 const messageTemplate = document.querySelector('#message-template');
+const typingBox = document.querySelector('.typing-box');
+const errorMessage = document.createElement('p');
+
+function createError() {
+    errorMessage.innerText = 'You need to enter text here';
+    errorMessage.setAttribute('data-validation', 'error');
+    errorMessage.classList.add('error');
+    typingBox.appendChild(errorMessage);
+}
+
+function removeError() {
+    const errorElement = document.querySelector('[data-validation]');
+
+    if (errorElement) {
+        const errorAttr = errorElement.getAttribute('data-validation');
+        if (errorAttr === 'error') {
+            errorElement.remove();
+        }
+    }
+}
 
 button.onclick = function () {
     const inputText = input.value;
     const inputTextReplace = inputText.replace(/^\s*/, '').replace(/\s*$/, '');
-    console.log(inputText);
-    console.log(inputTextReplace);
-
     if (inputText !== '') {
         const cloneTemplate = messageTemplate.content.cloneNode(true);
         const messageBody = cloneTemplate.querySelectorAll('[data-message]');
         messageBody[0].textContent = inputTextReplace;
         container.appendChild(cloneTemplate);
         input.value = '';
+        removeError();
     } else {
-        console.log('error');
+        createError();
     }
-
-    // const scrollinDiv = document.querySelector('.messages-box ');
-    // setInterval(function () {
-    //     scrollinDiv.scrollTop = 9999;
-    // }, 100);
 };
