9b27ba58ff3b99d4f1a99ec6ccbb7c1e1547355f
N_Yarovenko <mykolayarovenko@gmail.com>
2020-10-30 02:10:32 +0200

    add live chcat

---
+++ homeworks/mykola.yarovenko_MykYarovenko36/2-dom/index.js
@@ -0,0 +1,53 @@
+const clearInput = (item) => {
+    item.value = '';
+    return item;
+};
+const newDate = () => {
+    const date = new Date();
+    return date.toLocaleTimeString();
+};
+const messageTemplateNode = (message, date = newDate()) => {
+		const item = document.createElement('li');
+    const messageText = document.createElement('p');
+    messageText.innerText = message;
+    const messageDate = document.createElement('p');
+    messageDate.style.fontSize = '12px';
+    messageDate.innerText = `added: ${date}`;
+    item.append(...[messageText, messageDate]);
+    return item;
+};
+const messagesState = [{ message: 'hello, tell me something!?', date: '02:28:23' }, { message: 'hello, tell me something!?', date: '03:08:23' }, { message: 'Hey, Bro!', date: '05:18:23' }, { message: 'maybe a coffee break?', date: '04:08:23' }];
+messagesState.forEach((item) => {
+    document.querySelector('[chat]').append(messageTemplateNode(item.message, item.date));
+});
+function newMessage(event) {
+    const inputMessage = document.querySelector('[message-input]');
+    const sendCursor = {
+        message: 'sending!',
+        timeout: 2000,
+    };
+    const setCursor = (eventTypeText = 'typing...', timeout = 1000) => {
+        const typingCursor = document.querySelector('[typing]');
+        typingCursor.innerText = eventTypeText;
+        typingCursor.hidden = false;
+        setTimeout(() => {
+            typingCursor.hidden = true;
+        }, timeout);
+        return typingCursor;
+    };
+    setCursor();
+    if (event.key === 'Enter' || event.type === 'click') {
+        if (inputMessage.value === '') {
+            return inputMessage.value = '';
+					}
+            else {
+                const item = { message: `${inputMessage.value}`, date: newDate() };
+								messagesState.push(item);
+                document.querySelector('[chat]').prepend(messageTemplateNode(item.message, item.date));
+                setCursor(sendCursor.message, sendCursor.timeout);
+                clearInput(inputMessage);
+              }
+    }
+}
+document.querySelector('[submit-button]').addEventListener('click', newMessage);
+document.querySelector('[message-input]').addEventListener('keyup', newMessage);
