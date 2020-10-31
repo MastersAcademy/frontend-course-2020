2df0b5a918a31cd4aadc1426f76145690e7458d5
Olga Chotii <olgachoty@gmail.com>
2020-10-31 19:53:44 +0200

    2

--- homeworks/olga.chotiy_olgachotii/2-dom/index.js
+++ homeworks/olga.chotiy_olgachotii/2-dom/index.js
@@ -1,17 +1,17 @@
-var elem = document.querySelector('[name="elem"]');
-var textContent = document.querySelector('[name="textContent"]');
+const elem = document.querySelector('[name="elem"]');
+const textContent = document.querySelector('[name="textContent"]');
 elem.onclick = function (event) {
     event.preventDefault();
-    let text = textContent.value;
-    if (text) {       
-        const list = document.querySelector('ul');      
+    const text = textContent.value;
+    if (text) {
+        const list = document.querySelector('ul');
         const newItem = document.createElement('li');
-        newItem.textContent = text.replace(/ +/g, ' ').trim();      
-        list.appendChild(newItem);        
-    };
+        newItem.textContent = text.replace(/ +/g, ' ').trim();
+        list.appendChild(newItem);
+    }
     textContent.value = '';
 };
-   
+  
 
 
     
