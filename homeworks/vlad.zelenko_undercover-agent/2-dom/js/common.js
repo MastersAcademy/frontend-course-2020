a12f48c07b51af2a4ab684999183cc02a3bde5fc
VladZelenko <Vlad6555z@gmail.com>
2020-10-31 21:03:24 +0200

    added typing, validate

--- homeworks/vlad.zelenko_undercover-agent/2-dom/js/common.js
+++ homeworks/vlad.zelenko_undercover-agent/2-dom/js/common.js
@@ -2,27 +2,27 @@ const inputData = document.querySelector(".form-control");
 const sendButton = document.querySelector(".send-button");
 const messages = document.querySelector(".messages");
 const typingIndicator = document.querySelector(".typing");
+let timer;
 
 sendButton.addEventListener("click", (e) => sendForm(e) );
 
 function sendForm(event) {
     event.preventDefault();
-    outMessageFromScreen();
+    outMessageToScreen();
     inputData.value = "";
     typingIndicator.innerHTML = "";
 }
 
-function outMessageFromScreen() {
+function outMessageToScreen() {
   let div = document.createElement('div');
-  div.innerHTML =  inputData.value;
+  div.innerHTML =  inputData.value.trim();
   messages.appendChild(div);
 }
 
-// function verificationInputData() {
-//   let validate = /[!@#$%&*]/.test(inputData.value);
-// }
-
 inputData.oninput = function() {
   typingIndicator.innerHTML = "typing";
+  clearTimeout(timer);
+  timer = setTimeout(() => {
+    typingIndicator.innerHTML = "";
+  }, 1000);
 };
-
