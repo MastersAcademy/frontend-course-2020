13abe2c14aa780e1dabb03325dbc5e247bb1a16b
Olga Chotii <olgachoty@gmail.com>
2020-10-31 20:10:50 +0200

    Update index.js

--- homeworks/olga.chotiy_olgachotii/2-dom/index.js
+++ homeworks/olga.chotiy_olgachotii/2-dom/index.js
@@ -1,15 +1,15 @@
 const elem = document.querySelector('[name="elem"]');
 const textContent = document.querySelector('[name="textContent"]');
 elem.onclick = function (event) {
-event.preventDefault();
-const text = textContent.value;
-if (text) {
-const list = document.querySelector('ul');
-const newItem = document.createElement('li');
-newItem.textContent = text.replace(/ +/g, ' ').trim();
-list.appendChild(newItem);
-}
-textContent.value = '';
+    event.preventDefault();
+    const text = textContent.value;
+    if (text) {
+        const list = document.querySelector('ul');
+        const newItem = document.createElement('li');
+        newItem.textContent = text.replace(/ +/g, ' ').trim();
+        list.appendChild(newItem);
+    }
+    textContent.value = '';
 };
   
 
