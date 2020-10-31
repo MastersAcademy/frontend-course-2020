8b8782276eb8dd69231fe6112c140a2b6c4671b6
Olga Chotii <olgachoty@gmail.com>
2020-10-31 18:33:56 +0200

    working

---
+++ homeworks/olga.chotiy_olgachotii/1-intro/2-dom/index.js
@@ -0,0 +1,18 @@
+elem.onclick = function (event) {
+    event.preventDefault();
+    let text = textContent.value
+    if (text) {       
+        const list = document.querySelector('ul');      
+        const newItem = document.createElement('li');
+        newItem.textContent = text.replace(/ +/g, ' ').trim();      
+        list.appendChild(newItem);        
+    };
+    textContent.value = "";
+};
+   
+
+
+    
+           
+
+        
