5e15583330aeb898554f9607bdc8522d77e94497
spavel-gh <spavel.mail@gmail.com>
2020-11-01 01:44:32 +0200

    js manual fix with ESLint

--- homeworks/sokurenko.pavel_spavel_gh/2-dom/script.js
+++ homeworks/sokurenko.pavel_spavel_gh/2-dom/script.js
@@ -1,16 +1,68 @@
 let inputNode;
 let send1Node;
 let send2Node;
-let typing_indicatorNode;
+let typingIndicatorNode;
+
+function creater(btnID, text) {
+    if ('content' in document.createElement('template')) {
+        const avatarColorClass = ['color-one', 'color-two'];
+        const authorName = ['Tom', 'Jerry'];
+
+        const msgBodyNode = document.querySelector('[msg-body]');
+        const msgTmlpNode = document.querySelector('[msg-tmlp]');
+        const clone = msgTmlpNode.content.cloneNode(true);
+        const msgAvatar = clone.querySelector('.avatar');
+        const msgTitile = clone.querySelector('.msg-title');
+        const msgBody = clone.querySelector('.msg-body');
+
+        msgAvatar.classList.add(avatarColorClass[btnID]);
+        msgTitile.textContent = authorName[btnID];
+        msgBody.textContent = text;
+
+        msgBodyNode.appendChild(clone);
+        msgBodyNode.scrollTop = 65535;
+    } else alert('Ошибка! Браузер не поддерживает <template>');
+}
+
+function validator(text) {
+    if (text.replace(/\s+/g, '').length === 0) return false;
+    return text.replace(/\s+/g, ' ').trim();
+}
+
+function receiver(text, btnID) {
+    inputNode.value = null;
+    const validatedText = validator(text);
+    if (!validatedText) return;
+    creater(btnID, validatedText);
+}
+
+// hiding
+// Индикатор ввода
+let timerId;
+let typingStatus = false;
+function typingIndicator() {
+    if (typingStatus === false) {
+        typingStatus = true;
+        console.log('do start');
+        typingIndicatorNode.classList.remove('hidden');
+    }
+
+    clearTimeout(timerId);
+    timerId = setTimeout(() => {
+        typingStatus = false;
+        typingIndicatorNode.classList.add('hidden');
+        console.log('do stop');
+    }, 1000);
+}
 
 window.onload = function () {
     /* Инициализация */
     inputNode = document.querySelector('[msg-input]');
     send1Node = document.querySelector('[msg-send1]');
     send2Node = document.querySelector('[msg-send2]');
-    send1Node.addEventListener('click', (event) => receiver(inputNode.value, 0));
-    send2Node.addEventListener('click', (event) => receiver(inputNode.value, 1));
-    typing_indicatorNode = document.querySelector('[hidden]');
+    send1Node.addEventListener('click', () => receiver(inputNode.value, 0));
+    send2Node.addEventListener('click', () => receiver(inputNode.value, 1));
+    typingIndicatorNode = document.querySelector('[hidden]');
 
     // для сочетания клавиш (из инета)
     // https://www.gavsblog.com/blog/detect-single-and-multiple-keypress-events-javascript
@@ -23,67 +75,15 @@ window.onload = function () {
             || (event.which >= 48 && event.which <= 57)
             || (event.which >= 96 && event.which <= 111)
             || (event.which >= 187 && event.which <= 192)
-            || event.which == 32
-        ) typing_indicator();
+            || event.which === 32
+        ) typingIndicator();
 
         // отправляем через Enter или Ctrl+Enter
         keysPressed[event.key] = true;
-        if (!keysPressed.Control && event.key == 'Enter') receiver(inputNode.value, 0);
-        else if (keysPressed.Control && event.key == 'Enter') receiver(inputNode.value, 1);
+        if (!keysPressed.Control && event.key === 'Enter') receiver(inputNode.value, 0);
+        else if (keysPressed.Control && event.key === 'Enter') receiver(inputNode.value, 1);
     });
     inputNode.addEventListener('keyup', (event) => {
         delete keysPressed[event.key];
     });
 };
-
-// hiding
-// Индикатор ввода
-let timerId;
-let typingStatus = false;
-function typing_indicator() {
-    if (typingStatus == false) {
-        typingStatus = true;
-        console.log('do start');
-        typing_indicatorNode.classList.remove('hidden');
-    }
-
-    clearTimeout(timerId);
-    timerId = setTimeout(() => {
-        typingStatus = false;
-        typing_indicatorNode.classList.add('hidden');
-        console.log('do stop');
-    }, 1000);
-}
-
-function receiver(text, btnID) {
-    inputNode.value = null;
-    text = validator(text);
-    if (!text) return;
-    creater(btnID, text);
-}
-
-function validator(text) {
-    if (text.replace(/\s+/g, '').length == 0) return false;
-    return text.replace(/\s+/g, ' ').trim();
-}
-
-function creater(btnID, text) {
-    if ('content' in document.createElement('template')) {
-        const avatar_color_class = ['color-one', 'color-two'];
-        const author_name = ['Tom', 'Jerry'];
-
-        const msgBodyNode = document.querySelector('[msg-body]');
-        const msgTmlpNode = document.querySelector('[msg-tmlp]');
-        const clone = msgTmlpNode.content.cloneNode(true);
-        const msgAvatar = clone.querySelector('.avatar');
-        const msgTitile = clone.querySelector('.msg-title');
-        const msgBody = clone.querySelector('.msg-body');
-
-        msgAvatar.classList.add(avatar_color_class[btnID]);
-        msgTitile.textContent = author_name[btnID];
-        msgBody.textContent = text;
-
-        msgBodyNode.appendChild(clone);
-        msgBodyNode.scrollTop = 65535;
-    } else alert('Ошибка! Браузер не поддерживает <template>');
-}
