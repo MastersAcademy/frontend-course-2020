8218e9bcae44e5309a7b4a3e09ebbfb06204edc8
VladZelenko <Vlad6555z@gmail.com>
2020-10-31 22:52:29 +0200

    fixed errors eslint

--- homeworks/vlad.zelenko_undercover-agent/2-dom/js/common.js
+++ homeworks/vlad.zelenko_undercover-agent/2-dom/js/common.js
@@ -1,32 +1,32 @@
-const inputData = document.querySelector(".form-control");
-const sendButton = document.querySelector(".send-button");
-const messages = document.querySelector(".messages");
-const typingIndicator = document.querySelector(".typing");
+const inputData = document.querySelector('.form-control');
+const sendButton = document.querySelector('.send-button');
+const messages = document.querySelector('.messages');
+const typingIndicator = document.querySelector('.typing');
 let timer;
 
-sendButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  if (inputData.value != "") {
-    sendForm()
-  }
-});
-
-function sendForm(event) {
-  outMessageToScreen();
-  inputData.value = "";
-  typingIndicator.innerHTML = "";
+function outMessageToScreen() {
+    const div = document.createElement('div');
+    div.innerHTML = inputData.value.trim();
+    messages.appendChild(div);
 }
 
-function outMessageToScreen() {
-  let div = document.createElement('div');
-  div.innerHTML = inputData.value.trim();
-  messages.appendChild(div);
+function sendForm() {
+    outMessageToScreen();
+    inputData.value = '';
+    typingIndicator.innerHTML = '';
 }
 
 inputData.oninput = function () {
-  typingIndicator.innerHTML = "Typing...";
-  clearTimeout(timer);
-  timer = setTimeout(() => {
-    typingIndicator.innerHTML = "";
-  }, 1000);
+    typingIndicator.innerHTML = 'Typing...';
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+        typingIndicator.innerHTML = '';
+    }, 1000);
 };
+
+sendButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (inputData.value !== '') {
+        sendForm();
+    }
+});
