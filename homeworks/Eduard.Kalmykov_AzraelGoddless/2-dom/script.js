aa568eaf514bac26decc9fbb38d523bad4e51d18
JIexmark <jiexmark1@gmail.com>
2020-11-01 23:16:51 +0200

    Update script.js

--- homeworks/Eduard.Kalmykov_AzraelGoddless/2-dom/script.js
+++ homeworks/Eduard.Kalmykov_AzraelGoddless/2-dom/script.js
@@ -2,8 +2,8 @@ const inputIn = document.querySelector('.input-in');
 const button = document.querySelector('button');
 const ul = document.querySelector('.out');
 button.onclick = function () {
-    const a = document.createElement('li');
-    a.innerHTML = inputIn.value;
-    ul.appendChild(a);
+    const message = document.createElement('li');
+    message.innerHTML = inputIn.value;
+    ul.appendChild(message);
     inputIn.value = '';
 };
