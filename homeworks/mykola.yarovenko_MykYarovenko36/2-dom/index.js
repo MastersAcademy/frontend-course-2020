53e7dd275e2e77d209dc83090cbe2f4da89d63bf
N_Yarovenko <mykolayarovenko@gmail.com>
2020-11-01 04:48:07 +0200

    app fixed

--- homeworks/mykola.yarovenko_MykYarovenko36/2-dom/index.js
+++ homeworks/mykola.yarovenko_MykYarovenko36/2-dom/index.js
@@ -1,53 +1,74 @@
-const clearInput = (element) => {
-    const item = element;
-    item.value = '';
-    return item;
-};
-const newDate = () => {
-    const date = new Date();
-    return date.toLocaleTimeString();
-};
-const messageTemplateNode = (message, date = newDate()) => {
-    const item = document.createElement('li');
-    const messageText = document.createElement('p');
-    messageText.innerText = message.trim();
-    const messageDate = document.createElement('p');
-    messageDate.style.fontSize = '12px';
-    messageDate.innerText = `added: ${date}`;
-    item.append(...[messageText, messageDate]);
-    return item;
+const messageTemplateNode = (element) => {
+    const { message, date } = element;
+    const itemEl = document.createElement('li');
+    itemEl.className = 'dialogs_list_element';
+    itemEl.innerHTML = `
+        <p>${message.trim()}</p>
+        <p style="font-size: 12px">added: ${date}`;
+    return itemEl;
 };
 const messagesState = [{ message: 'hello, tell me something!?', date: '02:28:23' }, { message: 'hello, tell me something!?', date: '03:08:23' }, { message: 'Hey, Bro!', date: '05:18:23' }, { message: 'maybe a coffee break?', date: '04:08:23' }];
-messagesState.forEach((item) => {
-    document.querySelector('[chat]').append(messageTemplateNode(item.message, item.date));
+
+messagesState.map((item) => {
+    const dialogsElement = item;
+    document.querySelector('[data-dialogs-list]').append(messageTemplateNode(dialogsElement));
+    return dialogsElement;
 });
-function newMessage(event) {
-    const inputMessage = document.querySelector('[message-input]');
-    const sendCursor = {
-        message: 'sending!',
-        timeout: 2000,
-    };
-    const setCursor = (eventTypeText = 'typing...', timeout = 1000) => {
-        const typingCursor = document.querySelector('[typing]');
-        typingCursor.innerText = eventTypeText;
-        typingCursor.hidden = false;
+
+const newMessageDate = new Date().toLocaleTimeString();
+
+function InputEventPointer(obj) {
+    this.message = obj.message;
+    this.timer = obj.timer;
+    this.showPointer = () => {
+        const pointerMessage = this.message;
+        const pointerTimer = this.timer;
+        const pointer = document.querySelector('[data-message-status-pointer]');
+        pointer.innerText = pointerMessage;
+        pointer.hidden = false;
         setTimeout(() => {
-            typingCursor.hidden = true;
-        }, timeout);
-        return typingCursor;
+            pointer.hidden = true;
+        }, pointerTimer);
+        return pointer;
     };
-    setCursor();
+}
+const typingPointer = {
+    message: 'typing...',
+    timer: 1000,
+};
+const sendingPointer = {
+    message: 'sending...',
+    timer: 2000,
+};
+const emptyMessagePointer = {
+    message: 'message is empty',
+    timer: 1500,
+};
+const messageIsTyping = new InputEventPointer(typingPointer);
+const messageIsSend = new InputEventPointer(sendingPointer);
+const messageIsEmpty = new InputEventPointer(emptyMessagePointer);
+
+const postNewMessage = (textData) => {
+    const messageText = textData.value;
+    const itemEl = { message: `${messageText}`, date: newMessageDate };
+    messagesState.push(itemEl);
+    document.querySelector('[data-dialogs-list]').prepend(messageTemplateNode(itemEl));
+    return messagesState;
+};
+
+function messageFormHandler(event) {
+    const inputTextEl = document.querySelector('[data-message-input]');
     if (event.key === 'Enter' || event.type === 'click') {
-        if (inputMessage.value === '') {
-            event.preventDefault();
+        if (inputTextEl.value === '') {
+            messageIsEmpty.showPointer();
         } else {
-            const item = { message: `${inputMessage.value}`, date: newDate() };
-            messagesState.push(item);
-            document.querySelector('[chat]').prepend(messageTemplateNode(item.message, item.date));
-            setCursor(sendCursor.message, sendCursor.timeout);
-            clearInput(inputMessage);
+            postNewMessage(inputTextEl);
+            inputTextEl.value = '';
+            messageIsSend.showPointer();
         }
+    } else {
+        messageIsTyping.showPointer();
     }
 }
-document.querySelector('[submit-button]').addEventListener('click', newMessage);
-document.querySelector('[message-input]').addEventListener('keyup', newMessage);
+document.querySelector('[data-submit-button]').addEventListener('click', messageFormHandler);
+document.querySelector('[data-message-input]').addEventListener('keyup', messageFormHandler);
