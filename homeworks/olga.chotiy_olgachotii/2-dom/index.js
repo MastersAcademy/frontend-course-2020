1248541e8df8b597e2b877fe8e47f28c7901bc28
Olga Chotii <olgachoty@gmail.com>
2020-11-02 12:48:50 +0200

    work on bugs

--- homeworks/olga.chotiy_olgachotii/2-dom/index.js
+++ homeworks/olga.chotiy_olgachotii/2-dom/index.js
@@ -1,13 +1,15 @@
-const elem = document.querySelector('[name="elem"]');
-const textContent = document.querySelector('[name="textContent"]');
-elem.onclick = function (event) {
-    event.preventDefault();
+const elem = document.getElementById('elem');
+const textContent = document.getElementById('textContent');
+elem.addEventListener("click", function submit(event) {
+     event.preventDefault();
     const text = textContent.value;
     if (text) {
-        const list = document.querySelector('ul');
+        const list = document.getElementById('list');
         const newItem = document.createElement('li');
         newItem.textContent = text.replace(/ +/g, ' ').trim();
         list.appendChild(newItem);
+        document.body.children[0].lastElementChild.classList.add('article');
     }
     textContent.value = '';
-};
+});
+
