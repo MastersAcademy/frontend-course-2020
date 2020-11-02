0145d583b9306f149a4e6eb6ccc08c317a4f4b30
Maks Savchenko <maks.20050817@gmail.ru>
2020-11-02 22:09:46 +0200

    fixed 2-dom

--- homeworks/maks.savchenko_maksbtw/2-dom/script.js
+++ homeworks/maks.savchenko_maksbtw/2-dom/script.js
@@ -1,38 +1,34 @@
-const chat = document.querySelector('#chat');
-const subBtn = document.querySelector('#submit');
-const input = document.querySelector('#input');
+const chat = document.querySelector('.chat');
+const subBtn = document.querySelector('.submit');
+const input = document.querySelector('.input');
 
-function sending(e) {
-    const { value } = document.querySelector('#input');
+function sending() {
+    const value = document.querySelector('.input').value;
     input.value = '';
     if (value.trim()) {
         const display = `<span class="message">${value}</span>`;
         chat.innerHTML += display;
-        document.querySelector('#typing').classList.remove('toggle');
-    } else {
-
+        document.querySelector('.typing').classList.remove('toggle');
     }
 }
 
-subBtn.addEventListener('click', (e) => {
-    sending(e);
+subBtn.addEventListener('click', (event) => {
+    sending(event);
 });
-/* Shortcut for sending messages */
+// Shortcut for sending messages
 input.addEventListener('focus', (event) => {
     document.addEventListener('keydown', (e) => {
-        if (e.keyCode == 13) {
-            sending(e);
-        } else {
-
+        if (e.keyCode === 13) {
+            sending(event);
         }
     });
 });
 
-input.addEventListener('input', (e) => {
-    const { value } = document.querySelector('#input');
+input.addEventListener('input', () => {
+    const value = document.querySelector('.input').value;
     if (value.trim()) {
-        document.querySelector('#typing').classList.add('toggle');
+        document.querySelector('.typing').classList.add('toggle');
     } else {
-        document.querySelector('#typing').classList.remove('toggle');
+        document.querySelector('.typing').classList.remove('toggle');
     }
 });
