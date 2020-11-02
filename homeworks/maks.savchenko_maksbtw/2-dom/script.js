b4d75e32c22575150a177abf82e4282faf317bde
Maks Savchenko <maks.20050817@gmail.ru>
2020-11-02 20:34:53 +0200

    fixed js

--- homeworks/maks.savchenko_maksbtw/2-dom/script.js
+++ homeworks/maks.savchenko_maksbtw/2-dom/script.js
@@ -1,41 +1,38 @@
-const chat = document.querySelector('#chat')
-const subBtn = document.querySelector('#submit')
-const input = document.querySelector('#input')
+const chat = document.querySelector('#chat');
+const subBtn = document.querySelector('#submit');
+const input = document.querySelector('#input');
 
-function sending(e){
-	let value = document.querySelector('#input').value
-	input.value = ''
-	if(value.trim()){
-		let display = `<span class="message">${value}</span>`
-		chat.innerHTML += display
-		document.querySelector('#typing').classList.remove('toggle')
-	}
-	else {
+function sending(e) {
+    const { value } = document.querySelector('#input');
+    input.value = '';
+    if (value.trim()) {
+        const display = `<span class="message">${value}</span>`;
+        chat.innerHTML += display;
+        document.querySelector('#typing').classList.remove('toggle');
+    } else {
 
-	}
+    }
 }
 
-subBtn.addEventListener('click', function(e) {
-	sending(e)
-})
-/*Shortcut for sending messages*/
-input.addEventListener('focus', function(event) {
-	document.addEventListener('keydown', function(e) {
-		if(e.keyCode == 13){
-			sending(e)
-		}
-		else {
+subBtn.addEventListener('click', (e) => {
+    sending(e);
+});
+/* Shortcut for sending messages */
+input.addEventListener('focus', (event) => {
+    document.addEventListener('keydown', (e) => {
+        if (e.keyCode == 13) {
+            sending(e);
+        } else {
 
-		}
-	})
-})
+        }
+    });
+});
 
-input.addEventListener('input', function(e) {
-	let value = document.querySelector('#input').value
-	if(value.trim()){
-		document.querySelector('#typing').classList.add('toggle')
-	}
-	else {
-		document.querySelector('#typing').classList.remove('toggle')
-	}
-})
+input.addEventListener('input', (e) => {
+    const { value } = document.querySelector('#input');
+    if (value.trim()) {
+        document.querySelector('#typing').classList.add('toggle');
+    } else {
+        document.querySelector('#typing').classList.remove('toggle');
+    }
+});
