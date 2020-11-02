48d98af82e1aeb920b55a6eb4966d29896357989
David <davakpacaba13@gmail.com>
2020-11-02 19:38:28 +0200

    fixed

--- homeworks/david.tereshchenko_DavidTereshchenko/2-dom/index.js
+++ homeworks/david.tereshchenko_DavidTereshchenko/2-dom/index.js
@@ -5,7 +5,7 @@ const listNode = document.querySelector('.chat-list');
 function submitMessage(event){
     event.preventDefault();
     const vld = inputNode.value.trim();
-    if(vld.length) {
+    if (vld.length) {
         const addMessages = document.createElement('div');
         addMessages.classList.add('msg');
         addMessages.innerHTML = inputNode.value;
@@ -14,12 +14,12 @@ function submitMessage(event){
     }
 }
 
-butNode.addEventListener('click', submitMessage)
+butNode.addEventListener('click', submitMessage);
 
 inputNode.addEventListener('keyup', function(event) {
-    if(event.keyCode === 13) {
+    if (event.keyCode === 13) {
         event.preventDefault();
-        submitMessage(event)
+        submitMessage(event);
     }
 })
 
