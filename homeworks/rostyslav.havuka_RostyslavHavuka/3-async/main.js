08e6347704788db73e395f61e252a6db00303f8d
rostyslav <ronyahavuka@gmail.com>
2020-11-04 22:53:01 +0200

    Add sort

--- homeworks/rostyslav.havuka_RostyslavHavuka/3-async/main.js
+++ homeworks/rostyslav.havuka_RostyslavHavuka/3-async/main.js
@@ -4,11 +4,44 @@ document.addEventListener('DOMContentLoaded', async (event) => {
   const response = await fetch('https://jsonplaceholder.typicode.com/posts');
   data = await response.json();
   document.body.innerHTML = `<div class="loader"></div>`;
+
   await timeout(3000);
-  document.body.innerHTML = `
-${prepareDropDownMenu()} 
-${preparePost()}
-`;
+  document.body.innerHTML = `${prepareDropDownMenu()} ${preparePost()} `;
+  function renderPost() {
+    preparePost
+  }
+
+  let select = document.getElementById('mySelect')
+
+  select.onchange = () => {
+    let onSelect = document.getElementById("mySelect").value;
+    let computedData = data
+    switch (onSelect) {
+      case 'default':
+        document.getElementById('main').innerHTML = preparePost(computedData)
+        break;
+      case 'az':
+        data = data.sort(function(a, b){
+          var nameA=a.title.toLowerCase(), nameB=b.title.toLowerCase()
+          if (nameA < nameB)
+            return -1
+          if (nameA > nameB)
+            return 1
+        })
+          document.getElementById('main').innerHTML = preparePost(data)
+        break;
+      case 'za':
+        data = data.sort(function(a, b){
+          var nameA=a.title.toLowerCase(), nameB=b.title.toLowerCase()
+          if (nameA > nameB)
+            return -1
+          if (nameA < nameB)
+            return 1
+        })
+          document.getElementById('main').innerHTML = preparePost(data)
+        break;
+    }
+  }
 });
 
 function timeout(ms) {
@@ -17,23 +50,26 @@ function timeout(ms) {
 
 function prepareDropDownMenu() {
   return `
-<button class="dropbtn">Dropdown</button>
-<div class="dropdown-content">
-    <a href="#">Link 1</a>
-    <a href="#">Link 2</a>
-    <a href="#">Link 3</a>
-</div>
+  <select id="mySelect">
+  <option value="default">Default</option>
+  <option value="az">A-z</option>
+  <option value="za">Z-a</option>
+  </select>
 `
 };
 
+
 function preparePost() {
-  const dataArray = data.map(element => {
+  const dataArray = `<div class="main" id="main">
+  ${data.map(element => {
     return `
-<div class="main">
+    <div class="post">
 <p class="title">Title: ${element.title}</p>
 <p>${element.body}</p>
 </div>
 `
-  })
-  return dataArray.join('\n');
+  }).join('\n')}
+  </div>`
+  // console.log(data);
+  return dataArray;
 };
