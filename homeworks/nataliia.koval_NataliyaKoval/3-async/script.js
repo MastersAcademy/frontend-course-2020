60e0226f860ca9f6f7cdb4699d5fe65b2a5474c9
Nataliia Koval <hakunata@gmail.com>
2020-11-05 12:04:48 +0200

    dounloaded posts from API

---
+++ homeworks/nataliia.koval_NataliyaKoval/3-async/script.js
@@ -0,0 +1,19 @@
+async function getResponse() {
+  let response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  let posts = await response.json();
+
+  const list = document.querySelector('[data-list]');
+  const template = document.querySelector('[data-template]');
+  
+
+  for (let post of posts) {
+    const content = template.content;
+    const title = content.querySelector('[data-title]');
+    const text = content.querySelector('[data-text]');
+    title.innerText = post.title;
+    text.innerText = post.body;
+    list.appendChild(template.content.cloneNode(true));
+  }
+}
+setTimeout(getResponse, 3000);
+
