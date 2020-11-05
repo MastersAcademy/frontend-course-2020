023665ae8c80ced8bc827d8f6b9ffcc12cc5ed33
Anton Nemesh <anton.nemesh@gmail.com>
2020-11-06 00:13:53 +0200

    sort items

--- homeworks/anton.nemesh_AntonNemesh/3-async/main.js
+++ homeworks/anton.nemesh_AntonNemesh/3-async/main.js
@@ -1,10 +1,15 @@
+const containerEl = document.querySelector('[data-container]');
+const templateEl = document.querySelector('[data-template]');
+const buttonSort = document.querySelector('[data-sort]');
+const buttonReverse = document.querySelector('[data-reverse]');
+const buttonDefault = document.querySelector('[data-default]');
+
 function createPostItem(item, i) {
-    const containerEl = document.querySelector('[data-container]');
-    const templateEl = document.querySelector('[data-template]');
     const postClone = templateEl.content.cloneNode(true);
     const postTitle = postClone.querySelector('h2');
     const postBody = postClone.querySelector('p');
     const buttonDel = postClone.querySelector('button');
+
     postTitle.textContent = item.title;
     postBody.textContent = item.body;
     buttonDel.setAttribute(`data-del-${i}`, '');
@@ -25,13 +30,53 @@ function toggleLoader() {
     }
 }
 
-async function getData() {
+function deleteAllPosts() {
+    const elements = containerEl.getElementsByClassName('post');
+    while (elements[0]) {
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
+
+function sortUp(posts) {
+    return posts.sort((a, b) => {
+        const nameA = a.title.toLowerCase();
+        const nameB = b.title.toLowerCase();
+        if (nameA < nameB) {
+            return -1;
+        }
+        if (nameA > nameB) {
+            return 1;
+        }
+        return 0;
+    });
+}
+
+function sortDown(posts) {
+    return posts.sort((a, b) => {
+        const nameA = a.title.toLowerCase();
+        const nameB = b.title.toLowerCase();
+        if (nameA < nameB) {
+            return 1;
+        }
+        if (nameA > nameB) {
+            return -1;
+        }
+        return 0;
+    });
+}
+
+async function showData(sort) {
     const response = await fetch('https://jsonplaceholder.typicode.com/posts');
     const posts = await response.json();
+    deleteAllPosts();
+    if (sort === 'sort') { sortUp(posts); }
+    if (sort === 'reverse') { sortDown(posts); }
     posts.forEach((item, i) => {
         createPostItem(item, i);
     });
-    toggleLoader();
 }
 
-setTimeout(getData, 1000);
+buttonSort.addEventListener('click', () => { showData('sort').then(); });
+buttonReverse.addEventListener('click', () => { showData('reverse').then(); });
+buttonDefault.addEventListener('click', () => { showData().then(); });
+setTimeout(() => { showData().then(toggleLoader); }, 1000);
