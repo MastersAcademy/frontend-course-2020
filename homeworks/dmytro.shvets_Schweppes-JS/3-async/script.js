04c6ca7a31ea673bd8e228a50158145cc59694ce
Schweppes-JS <shvetsdmytro90@ukr.net>
2020-11-05 10:01:02 +0200

    created render function

--- homeworks/dmytro.shvets_Schweppes-JS/3-async/script.js
+++ homeworks/dmytro.shvets_Schweppes-JS/3-async/script.js
@@ -1,18 +1,48 @@
-const titles = document.querySelectorAll('[data-title]');
-const deleteBtn = document.querySelectorAll('[data-delete-button]');
-const text = document.querySelectorAll('[data-text]');
-
+const middleContainer = document.querySelector('[data-middle-container]');
 const posts = document.querySelectorAll('[data-post]');
 
 // Deleting preloading animation
 (function stoppingAnimation() {
-   let timer = setTimeout(() => {
-      posts.forEach(post => post.remove());
-      clearTimeout(timer);
-   }, 3000);
-})();
+    const timer = setTimeout(() => {
+        posts.forEach((post) => post.remove());
+        middleContainer.style.overflowY = 'auto';
+        clearTimeout(timer);
+    }, 3000);
+}());
 
+// Rendering posts
 (async function setPosts() {
-   const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-   const posts = await response.json();
-})();
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await response.json();
+    data.forEach((post) => {
+        // Creating cell fot post
+        const postNode = document.createElement('div');
+        postNode.classList.add('post');
+        postNode.setAttribute('data-post', '');
+        middleContainer.append(postNode);
+        // Creating title container
+        const titleContainerNode = document.createElement('div');
+        titleContainerNode.classList.add('title-container');
+        postNode.append(titleContainerNode);
+
+        const h2Node = document.createElement('h2');
+        h2Node.classList.add('h2');
+        h2Node.setAttribute('data-title', '');
+        h2Node.innerText = post.title;
+        titleContainerNode.append(h2Node);
+
+        const deleteBtnNode = document.createElement('i');
+        deleteBtnNode.classList.add('fas', 'fa-times-circle');
+        titleContainerNode.append(deleteBtnNode);
+        // Creating text container
+        const textContainerNode = document.createElement('div');
+        textContainerNode.classList.add('text-container');
+        postNode.append(textContainerNode);
+
+        const textNode = document.createElement('p');
+        textNode.classList.add('text');
+        textNode.setAttribute('data-text', '');
+        textNode.innerHTML = post.body;
+        textContainerNode.append(textNode);
+    });
+}());
