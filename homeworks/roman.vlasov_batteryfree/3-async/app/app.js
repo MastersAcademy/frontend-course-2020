948b9e6031f83acdbfe69c5aaa8083928543925e
batteryfree <battery.free@gmail.com>
2020-11-06 09:03:32 +0200

    reconstructed the code in the app.js file; renamed classes in index.html and style.css files; the list item was moved to a separate template and templates were moved to the root block.

--- homeworks/roman.vlasov_batteryfree/3-async/app/app.js
+++ homeworks/roman.vlasov_batteryfree/3-async/app/app.js
@@ -1,104 +1,139 @@
-const el = document.querySelector('#root');
-const controls = document.querySelector('[data-ctrls-tmp]').content.querySelector('[data-ctrls]');
-const input = controls.querySelector('[data-filter]');
-const post = document.querySelector('[data-ctrls-tmp]').content.querySelector('[data-post]');
-const listPosts = document.createElement('ul');
-const selectSort = controls.querySelector('[data-sort]');
+class App {
+    constructor(el) {
+        this.el = el;
+        this.posts = [];
+        this.filtredPosts = [];
+        this.findElements();
+        this.initRequest();
+        this.listenEvents();
+        this.sortToggle = null;
+    }
 
-listPosts.setAttribute('class', 'container posts');
+    findElements() {
+        const template = document.importNode(this.el.querySelector('[data-controls-template]').content, true);
+        this.elements = {
+            root: this.el,
+            controls: template.querySelector('[data-controls]'),
+            input: template.querySelector('[data-filter]'),
+            selectSort: template.querySelector('[data-sort]'),
+            postTemplate: this.el.querySelector('[data-item-template]').content,
+            listPosts: document.createElement('ul'),
+        };
+    }
 
-let posts = [];
-let filtredPosts = [];
-let sortToggle;
+    listenEvents() {
+        this.elements.input.addEventListener('input', () => {
+            this.removePosts();
+            this.appendPosts();
+        });
 
-function sortPosts() {
-    switch (sortToggle) {
-        case '2':
-            filtredPosts.sort((post1, post2) => {
-                if (post1.title < post2.title) return -1;
-                return 0;
+        this.elements.selectSort.addEventListener('change', (event) => {
+            this.sortToggle = event.target.value;
+            this.removePosts();
+            this.appendPosts();
+        });
+    }
+
+    initRequest() {
+        setTimeout(() => {
+            this.getPosts();
+        }, 0);
+    }
+
+    getPosts() {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then((response) => {
+                if (response.status !== 200) alert('Something went wrong');
+                return response.json();
+            })
+            .then((json) => {
+                this.posts = json;
+                this.removeLoader();
+                this.appendControls();
+                this.appendPosts();
+            })
+            .catch((error) => { alert(error); });
+    }
+
+    removeLoader() {
+        this.elements.root.removeChild(this.elements.root.firstElementChild);
+    }
+
+    appendControls() {
+        this.elements.root.appendChild(this.elements.controls);
+    }
+
+    removePosts() {
+        this.elements.root.removeChild(this.elements.root.querySelector('[data-posts]'));
+    }
+
+    searchPosts(input) {
+        this.filtredPosts = [].concat(this.posts.filter((post) => {
+            const result = post.title.toLowerCase().match(input.value.toLowerCase());
+            return result;
+        }));
+    }
+
+    sortPosts() {
+        switch (this.sortToggle) {
+            case '2':
+                this.filtredPosts.sort((post1, post2) => {
+                    if (post1.title < post2.title) return -1;
+                    return 0;
+                });
+                break;
+            case '3':
+                this.filtredPosts.sort((post1, post2) => {
+                    if (post1.title > post2.title) return -1;
+                    return 0;
+                });
+                break;
+            default:
+            // do nothing
+        }
+    }
+
+    createListPosts(posts) {
+        const cloneElementPosts = document.importNode(this.elements.listPosts, true);
+
+        cloneElementPosts.classList.add('container', 'posts');
+        cloneElementPosts.setAttribute('data-posts', '');
+
+        posts.forEach((element) => {
+            const cloneElementPost = document.importNode(this.elements.postTemplate, true);
+            const buttonDelPost = cloneElementPost.querySelector('[data-post-del]');
+
+            cloneElementPost.querySelector('[data-post-header]').innerHTML = element.title;
+            cloneElementPost.querySelector('[data-post-txt]').innerHTML = element.body;
+            cloneElementPost.querySelector('[data-post]').dataset.post = element.id;
+
+            buttonDelPost.addEventListener('click', () => {
+                buttonDelPost.parentNode.style.display = 'none';
+                fetch(`https://jsonplaceholder.typicode.com/posts/${element.id}`, { method: 'DELETE' })
+                    .then((response) => {
+                        if (response.status === 200) {
+                            this.posts.splice(this.posts.indexOf(element), 1);
+                            this.removePosts();
+                            this.appendPosts();
+                        } else {
+                            buttonDelPost.parentNode.style.display = 'block';
+                            alert('Something went wrong');
+                        }
+                    }).catch(() => { alert('Something went wrong'); });
             });
-            break;
-        case '3':
-            filtredPosts.sort((post1, post2) => {
-                if (post1.title > post2.title) return -1;
-                return 0;
-            });
-            break;
-        default:
-        // do nothing
+
+            cloneElementPosts.appendChild(cloneElementPost);
+        });
+        return cloneElementPosts;
+    }
+
+    appendPosts() {
+        this.searchPosts(this.elements.input);
+        this.sortPosts();
+        const posts = this.createListPosts(this.filtredPosts);
+        this.elements.root.appendChild(posts);
     }
 }
 
-function removePosts() {
-    if (listPosts.firstChild) listPosts.innerHTML = '';
-}
-
-function searchPosts(element) {
-    filtredPosts = [].concat(posts.filter((postItem) => {
-        const result = postItem.title.toLowerCase().match(element.value.toLowerCase());
-        return result;
-    }));
-}
-
-function createListPosts(data) {
-    removePosts();
-    data.forEach((element) => {
-        const cloneElement = document.importNode(post, true);
-        cloneElement.dataset.post = element.id;
-        cloneElement.querySelector('[data-post-header]').innerHTML = element.title;
-        cloneElement.querySelector('[data-post-txt]').innerHTML = element.body;
-        const buttonDel = cloneElement.querySelector('[data-post-del]');
-        buttonDel.addEventListener('click', () => {
-            buttonDel.parentNode.style.display = 'none';
-            const id = buttonDel.parentNode.dataset.post;
-            fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, { method: 'DELETE' })
-                .then((response) => {
-                    if (response.status === 200) {
-                        posts.splice(posts.indexOf(element), 1);
-                        searchPosts(input);
-                        sortPosts();
-                        createListPosts(filtredPosts);
-                    } else {
-                        buttonDel.parentNode.style.display = 'block';
-                        alert('Something went wrong');
-                    }
-                }).catch(() => { alert('Something went wrong'); });
-        });
-        listPosts.appendChild(cloneElement);
-    });
-    el.appendChild(listPosts);
-}
-
-function getPosts() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-        .then((response) => {
-            if (response.status !== 200) alert('Something went 11wrong');
-            return response.json();
-        })
-        .then((json) => {
-            if (json) {
-                posts = json;
-                el.removeChild(el.firstElementChild);
-                el.appendChild(controls);
-                createListPosts(posts);
-            }
-        }).catch((error) => { alert(error); });
-}
-
-setTimeout(() => {
-    getPosts();
-}, 5000);
-
-input.addEventListener('input', () => {
-    searchPosts(input);
-    sortPosts();
-    createListPosts(filtredPosts);
-});
-
-selectSort.addEventListener('change', (event) => {
-    sortToggle = event.target.value;
-    searchPosts(input);
-    sortPosts();
-    createListPosts(filtredPosts);
-});
+const app = new App(document.querySelector('#root'));
+console.dir(app);
