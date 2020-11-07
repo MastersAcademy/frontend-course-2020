a386731540c07d212302d589e3d44e0f0ae53974
boris <boris@example.com>
2020-11-07 16:01:22 +0200

    change file

--- homeworks/eva.pavlenko_Evapavl/3-async/script/script.js
+++ homeworks/eva.pavlenko_Evapavl/3-async/script/script.js
@@ -1,35 +1,28 @@
 function start() {
-    const select = document.querySelector('.select');
-    const inputFilter = document.querySelector('.input_filter');
-    const postsNode = document.querySelector('.posts');
-    const loadingNode = document.querySelector('.loading');
+    const selectEl = document.querySelector('[data-select]');
+    const inputFilterEl = document.querySelector('[data-input-filter]');
+    const postsNode = document.querySelector('[data-posts]');
+    const loadingNode = document.querySelector('[data-loading]');
 
     const defaultUrl = 'https://jsonplaceholder.typicode.com/posts?';
-    const fromAtoZ = 'https://jsonplaceholder.typicode.com/posts?_sort=title&_order=asc';
-    const fromZtoA = 'https://jsonplaceholder.typicode.com/posts?_sort=title&_order=desc';
+    const fromAtoZ = `${defaultUrl}_sort=title&_order=asc`;
+    const fromZtoA = `${defaultUrl}_sort=title&_order=desc`;
 
     let timerId;
     let timerPreloader;
 
-    function drawPosts(title, body, id) {
-        const postNode = document.createElement('div');
-        postNode.classList.add('post');
-        postNode.setAttribute('data-id', `${id}`);
-        postsNode.appendChild(postNode);
-        const titleNode = document.createElement('h2');
-        titleNode.classList.add('title');
-        const descriptionNode = document.createElement('div');
-        descriptionNode.classList.add('description');
-        titleNode.textContent = title;
-        postNode.appendChild(titleNode);
-        descriptionNode.textContent = body;
-        postNode.appendChild(descriptionNode);
+    function drawPosts(posts) {
+        const rows = posts.map(({ body, title, id }) => `<div data-${id} class="post">
+            <h2 class="title">${title}</h2>
+            <div class="description">${body}</div>
+        </div>`).join('');
+        postsNode.innerHTML = rows;
     }
 
     async function getPosts(url) {
         const response = await fetch(url);
         const posts = await response.json();
-        posts.map((post) => drawPosts(post.title, post.body, post.id));
+        drawPosts(posts);
     }
 
     function stopPreloader() {
@@ -45,25 +38,31 @@ function start() {
     function sorting(query) {
         const posts = document.querySelectorAll('.post');
         posts.forEach((post) => post.remove());
-        if (select.value === 'default') {
-            getPosts(`${defaultUrl}${query}`);
-        } else if (select.value === 'fromAtoZ') {
-            getPosts(`${fromAtoZ}${query}`);
-        } else if (select.value === 'fromZtoA') {
-            getPosts(`${fromZtoA}${query}`);
+        switch (selectEl.value) {
+            case 'default':
+                getPosts(`${defaultUrl}${query}`);
+                break;
+            case 'fromAtoZ':
+                getPosts(`${fromAtoZ}${query}`);
+                break;
+            case 'fromZtoA':
+                getPosts(`${fromZtoA}${query}`);
+                break;
+            default:
+                break;
         }
     }
 
     function filterPosts() {
         clearTimeout(timerId);
         timerId = setTimeout(() => {
-            const query = `&title_like=${inputFilter.value}`;
+            const query = `&title_like=${inputFilterEl.value}`;
             sorting(query);
         }, 3000);
     }
 
-    select.addEventListener('change', filterPosts);
-    inputFilter.addEventListener('input', filterPosts);
+    selectEl.addEventListener('change', filterPosts);
+    inputFilterEl.addEventListener('input', filterPosts);
 }
 
 start();
