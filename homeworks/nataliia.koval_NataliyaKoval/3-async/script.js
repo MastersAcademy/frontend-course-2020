29757f0bd72c97d1c1982f02600098ebe8d59724
Nataliia Koval <hakunata@gmail.com>
2020-11-07 20:03:06 +0200

    added filter

--- homeworks/nataliia.koval_NataliyaKoval/3-async/script.js
+++ homeworks/nataliia.koval_NataliyaKoval/3-async/script.js
@@ -1,10 +1,11 @@
-async function getResponse() {
-  let response = await fetch('https://jsonplaceholder.typicode.com/posts');
-  let posts = await response.json();
+const preloader = document.querySelector('[data-preloader]');
+const list = document.querySelector('[data-list]');
+const template = document.querySelector('[data-template]');
+const filterInput = document.querySelector('[data-filter]');
 
-  const list = document.querySelector('[data-list]');
-  const template = document.querySelector('[data-template]');
-  
+async function getResponse() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts = await response.json();
 
   for (let post of posts) {
     const content = template.content;
@@ -15,9 +16,28 @@ async function getResponse() {
     list.appendChild(template.content.cloneNode(true));
   }
 
-  document.body.classList.add('loaded');
+  preloader.style.display = 'none';
+
+  postsAZ = posts.slice().sort(function(a, b) {
+    if (a.title.toLowerCase() < b.title.toLowerCase()) return -1;
+    if (a.title.toLowerCase() > b.title.toLowerCase()) return 1;
+    return 0;
+  });
+
+  postsZA = postsAZ.slice().reverse();
 }
+
 setTimeout(getResponse, 3000);
 
+filterInput.addEventListener('keyup', function() {
+  let filter = filterInput.value.toLowerCase();
+  filterElements = document.querySelectorAll('[data-list] li h2');
 
-
+  filterElements.forEach(element => {
+    if (element.innerText.toLowerCase().indexOf(filter) > -1) {
+      element.parentElement.style.display = '';
+    } else {
+      element.parentElement.style.display = 'none';
+    }
+  })
+});
