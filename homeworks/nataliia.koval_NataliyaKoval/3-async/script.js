a167f6d7da3279eb0fccd07d4f510cbdeda68bb0
Nataliia Koval <hakunata@gmail.com>
2020-11-07 21:15:41 +0200

    fix

--- homeworks/nataliia.koval_NataliyaKoval/3-async/script.js
+++ homeworks/nataliia.koval_NataliyaKoval/3-async/script.js
@@ -4,40 +4,35 @@ const template = document.querySelector('[data-template]');
 const filterInput = document.querySelector('[data-filter]');
 
 async function getResponse() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-  const posts = await response.json();
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const posts = await response.json();
 
-  for (let post of posts) {
-    const content = template.content;
-    const title = content.querySelector('[data-title]');
-    const text = content.querySelector('[data-text]');
-    title.innerText = post.title;
-    text.innerText = post.body;
-    list.appendChild(template.content.cloneNode(true));
-  }
+    // eslint-disable-next-line no-restricted-syntax
+    for (const post of posts) {
+        const { content } = template;
+        const title = content.querySelector('[data-title]');
+        const text = content.querySelector('[data-text]');
+        title.innerText = post.title;
+        text.innerText = post.body;
+        list.appendChild(template.content.cloneNode(true));
+    }
 
-  preloader.style.display = 'none';
-
-  postsAZ = posts.slice().sort(function(a, b) {
-    if (a.title.toLowerCase() < b.title.toLowerCase()) return -1;
-    if (a.title.toLowerCase() > b.title.toLowerCase()) return 1;
-    return 0;
-  });
-
-  postsZA = postsAZ.slice().reverse();
+    preloader.style.display = 'none';
 }
 
 setTimeout(getResponse, 3000);
 
-filterInput.addEventListener('keyup', function() {
-  let filter = filterInput.value.toLowerCase();
-  filterElements = document.querySelectorAll('[data-list] li h2');
+filterInput.addEventListener('keyup', () => {
+    const filter = filterInput.value.toLowerCase();
+    const filterElements = document.querySelectorAll('[data-list] li h2');
 
-  filterElements.forEach(element => {
-    if (element.innerText.toLowerCase().indexOf(filter) > -1) {
-      element.parentElement.style.display = '';
-    } else {
-      element.parentElement.style.display = 'none';
-    }
-  })
+    filterElements.forEach((element) => {
+        if (element.innerText.toLowerCase().indexOf(filter) > -1) {
+            // eslint-disable-next-line no-param-reassign
+            element.parentElement.style.display = '';
+        } else {
+            // eslint-disable-next-line no-param-reassign
+            element.parentElement.style.display = 'none';
+        }
+    });
 });
