6d6868139e658116aa3dba87d178a3fa684c6772
VladZelenko <Vlad6555z@gmail.com>
2020-11-10 22:01:03 +0200

    Fiexd errors js-linter

--- homeworks/vlad.zelenko_undercover-agent/3-async/js/common.js
+++ homeworks/vlad.zelenko_undercover-agent/3-async/js/common.js
@@ -1,66 +1,70 @@
 const URL = 'https://jsonplaceholder.typicode.com/posts';
 const postContainer = document.querySelector('.post-container');
 const formSortPosts = document.querySelector('.sort-post');
+const inputFilter = document.querySelector('.filter');
 
-function renderingElements(data) {
-    for (const val in data) {
-        const post = `
-        <div class="post">
-        <h1 class="post-title">${data[val].title}</h1>
-        <p>${data[val].body}</p>
-        </div>
-        `;
-        postContainer.insertAdjacentHTML('afterbegin', post);
-    }
+function filterValues(data) {
+    return data.filter((val) => val.title.includes(inputFilter.value));
 }
 
-// function filterPosts(data) {
-//     for (const val in data) {
-
-//     }
-// }
-
-
 function sortAZ(data) {
-    data.sort((a, b) => {
-        return a.title === b.title ? 0 : +(a.title < b.title) || -1;
-    });
-    renderingElements(data);
+    return [...data].sort((a, b) => (a.title === b.title ? 0 : +(a.title > b.title) || -1));
 }
 
 function sortZA(data) {
-    data.sort((a, b) => {
-        return a.title === b.title ? 0 : +(a.title > b.title) || -1;
-    });
-    renderingElements(data);
+    return [...data].sort((a, b) => (a.title === b.title ? 0 : +(a.title < b.title) || -1));
 }
 
-function sortPost(data) {
+function sortValues(data) {
+    switch (formSortPosts.value) {
+        case 'ZA':
+            return sortZA(data);
+        case 'AZ':
+            return sortAZ(data);
+        default:
+            return data;
+    }
+}
+
+function renderingElements(response) {
+    const data = sortValues(filterValues(response));
+    postContainer.innerHTML = data.map((val) => `
+        <div class="post">
+        <h1 class="post-title">${val.title}</h1>
+        <p>${val.body}</p>
+        </div>
+    `).join('');
+
+    return response;
+}
+
+function setEvents(data) {
     formSortPosts.addEventListener('change', () => {
-        switch (formSortPosts.value) {
-            case 'ZA':
-                sortZA(data);
-                break;
-            case 'AZ':
-                sortAZ(data);
-                break;
-            default:
-                requestToServer();
-                break;
-        }
+        renderingElements(data);
+    });
+    inputFilter.addEventListener('input', () => {
+        renderingElements(data);
     });
 }
+
+const status = function (response) {
+    if (response.status !== 200) {
+        return Promise.reject(new Error(response.statusText));
+    }
+    return Promise.resolve(response);
+};
+
+const json = function (response) {
+    return response.json();
+};
 
 async function requestToServer() {
-    try {
-        const response = await fetch(URL);
-        const data = await response.json();
-        renderingElements(data);
-        sortPost(data);
-    }
-    catch (e) {
-        console.error(e);
-    }
+    await fetch(URL)
+        .then(status)
+        .then(json)
+        .then(renderingElements)
+        .then(setEvents)
+        .catch((error) => { console.log('error', error); });
 }
 
 setTimeout(() => {
