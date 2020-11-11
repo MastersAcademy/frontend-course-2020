42334afc1f02c90160e24abe67fa1a0d837336f2
Anton Nemesh <anton.nemesh@gmail.com>
2020-11-11 18:36:16 +0200

    base of Modules task

---
+++ homeworks/anton.nemesh_AntonNemesh/4-modules/js/time.js
@@ -0,0 +1,85 @@
+function getDaysInMonth(date) {
+    return 32 - new Date(date.getFullYear(), date.getMonth(), 32).getDate();
+}
+
+function getFridaysOfMonth(date) {
+    const inputDate = new Date(date);
+    const fridays = [];
+    const myDate = new Date(inputDate.getFullYear(), inputDate.getMonth(), 1);
+    const daysInMonth = getDaysInMonth(myDate);
+    for (let i = 1; i <= daysInMonth; i++) {
+        let currentDate = myDate.getDate();
+        if (myDate.getDay() === 5) {
+            fridays.push(myDate.getDate());
+        }
+        currentDate += 1;
+        myDate.setDate(currentDate);
+    }
+    return fridays;
+}
+
+function isMonthLong(date) {
+    if (toString.call(date) !== '[object String]') {
+        console.error('isMonthLong: incorrect input format Date');
+    }
+    const myDate = new Date(date);
+    return getDaysInMonth(myDate) === 31;
+}
+
+function fullWeeksNumberInMonth(date) {
+    const inputDate = new Date(date);
+    const beginningOfTheMonth = new Date(inputDate.getFullYear(), inputDate.getMonth(), 1);
+    const daysInMonth = getDaysInMonth(beginningOfTheMonth);
+    const currentMonth = beginningOfTheMonth.getMonth();
+    let fullWeeks = 0;
+    for (let i = 1; i <= daysInMonth; i++) {
+        let currentDate = beginningOfTheMonth.getDate();
+        if (beginningOfTheMonth.getDay() === 1) {
+            const mondaysDate = beginningOfTheMonth;
+            mondaysDate.setDate(mondaysDate.getDate() + 6);
+            if (mondaysDate.getMonth() === currentMonth) {
+                fullWeeks += 1;
+            }
+        }
+        currentDate += 1;
+        beginningOfTheMonth.setDate(currentDate);
+    }
+    return fullWeeks;
+}
+
+function shortestWeekDaysNumber(date) {
+    const inputDate = new Date(date);
+    const beginningOfTheMonth = new Date(inputDate.getFullYear(), inputDate.getMonth(), 1);
+    const daysInMonth = getDaysInMonth(beginningOfTheMonth);
+    const weeks = [];
+    weeks[1] = [];
+    weeks[2] = [];
+    weeks[3] = [];
+    weeks[4] = [];
+    weeks[5] = [];
+    weeks[6] = [];
+    const result = [];
+    let numbWeek = 1;
+    for (let i = 1; i <= daysInMonth; i++) {
+        let currentDate = beginningOfTheMonth.getDate();
+        if (beginningOfTheMonth.getDay() === 1) {
+            numbWeek += 1;
+        }
+        weeks[numbWeek].push(beginningOfTheMonth.getDay());
+        currentDate += 1;
+        beginningOfTheMonth.setDate(currentDate);
+    }
+    weeks.forEach((value) => {
+        if (value.length !== 0) {
+            result.push(value.length);
+        }
+    });
+    return Math.min(...result);
+}
+
+export {
+    getFridaysOfMonth,
+    isMonthLong,
+    fullWeeksNumberInMonth,
+    shortestWeekDaysNumber,
+};
