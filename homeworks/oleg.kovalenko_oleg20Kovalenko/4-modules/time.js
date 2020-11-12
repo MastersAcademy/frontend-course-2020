93e2aee73d402b4db54488c9eb677c6c992d09bb
Oleg Kovalenko <oleg95kovalenko@gmail.com>
2020-11-12 13:05:41 +0200

    4-modules

---
+++ homeworks/oleg.kovalenko_oleg20Kovalenko/4-modules/time.js
@@ -0,0 +1,59 @@
+/* eslint-disable no-use-before-define */
+// eslint-disable-next-line no-unused-vars
+export function getFridaysOfMonth(date) {
+    const friday = [];
+    // eslint-disable-next-line no-undef
+    for (let i = 1; i <= getDaysOfMonth(date); i++) {
+        // eslint-disable-next-line no-undef
+        const dateFridaysOfMonth = new Date(date.getFullYear(), date.getMonth(), i);
+        if (dateFridaysOfMonth.getDay() === 5) {
+            friday.push(dateFridaysOfMonth.getDate());
+        }
+    }
+    return friday;
+}
+
+function getDaysOfMonth(date) {
+    return 32 - new Date(date.getFullYear(), date.getMonth(), 32).getDate();
+}
+
+// eslint-disable-next-line no-unused-vars
+export function isMonthLong(date) {
+    // eslint-disable-next-line no-undef
+    return getDaysOfMonth(date) === 31;
+}
+
+// eslint-disable-next-line no-unused-vars
+export function shortestWeekDaysNumber(date) {
+    // eslint-disable-next-line no-undef
+    const weekDaysNumber = getDaysOfMonth(date);
+    let weekDays = 0;
+    let daysNumber = 7;
+    for (let i = 1; i <= weekDaysNumber; i++) {
+        // eslint-disable-next-line no-undef
+        const dateFridaysOfMonth = new Date(date.getFullYear(), date.getMonth(), i);
+        // eslint-disable-next-line no-cond-assign
+        if (dateFridaysOfMonth.getDay() === 1 ? daysNumber > weekDays : daysNumber = weekDays) {
+            weekDays = 0;
+        }
+        weekDays++;
+    }
+    return daysNumber;
+}
+
+export function fullWeeksNumberInMonth(date) {
+    let fullWeeksInMonth = 0;
+    // eslint-disable-next-line no-shadow
+    const getDaysOfMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+
+    for (let i = 1; i <= getDaysOfMonth; i++) {
+        const newData = new Date(date.getFullYear(), date.getMonth(), i);
+        if (newData.getDay() === 1) {
+            newData.setDate(newData.getDate() + 6);
+            if (newData.getMonth() === date.getMonth()) {
+                fullWeeksInMonth++;
+            }
+        }
+    }
+    return fullWeeksInMonth;
+}
