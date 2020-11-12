a3cd6e3947dfc39b2a31a29c194018ba688e2e79
Schweppes-JS <shvetsdmytro90@ukr.net>
2020-11-12 15:36:04 +0200

    fixed errors

--- homeworks/dmytro.shvets_Schweppes-JS/4-modules/script.js
+++ homeworks/dmytro.shvets_Schweppes-JS/4-modules/script.js
@@ -1,64 +1,97 @@
-import { getFridaysOfMonth, isMonthLong, shortestWeekDaysNumber, fullWeeksNumberInMonth } from './time.js';
+import {
+    getFridaysOfMonth,
+    isMonthLong,
+    shortestWeekDaysNumber,
+    fullWeeksNumberInMonth,
+    calculationTimeZone,
+} from './time';
 
-(function() {
-  const formNode = document.querySelector('[data-form]');
-  const inputDateNode = document.querySelector('[data-input]');
-  const inputZoneNode = document.querySelector('[data-select]');
-  const isLongMonthNode = document.querySelector('[data-is-long]');
-  const amountFridaysNode = document.querySelector('[data-amount-fridays]');
-  const sizeWeekNode = document.querySelector('[data-size-week]');
-  const weeksAmountNode = document.querySelector('[data-weeks-amount]');
-  const timeNode = document.querySelector('[data-time]');
+(function () {
+    const formNode = document.querySelector('[data-form]');
+    const inputDateNode = document.querySelector('[data-input]');
+    const inputZoneNode = document.querySelector('[data-select]');
+    const isLongMonthNode = document.querySelector('[data-is-long]');
+    const amountFridaysNode = document.querySelector('[data-amount-fridays]');
+    const sizeWeekNode = document.querySelector('[data-size-week]');
+    const weeksAmountNode = document.querySelector('[data-weeks-amount]');
+    const timeNode = document.querySelector('[data-time]');
 
-  let currentTime = setInterval(() => {
-    const hours = new Date().getHours();
-    const minutes = new Date().getMinutes();
-    timeNode.innerText = `${hours < 10 ? `0${hours}` : hours}  : ${minutes < 10 ? `0${minutes}` : minutes}`;
-  }, 1000);
-  let value = '';
-  let date = '';
+    let timer;
+    let timeDifference; // in Greenwich Mean Time
+    let timeZoneTime;
+    let date;
+    let hours;
+    let minutes;
+    let value = '';
+    let today;
 
-  // Setting defaults texts when user chose another date
-  function clearingNodesValue() {
-    amountFridaysNode.innerText = '';
-    isLongMonthNode.innerText = 'Unknown';
-    sizeWeekNode.innerText = '';
-    weeksAmountNode.innerText = '';
-  }
+    // Setting defaults texts when user chose another date
+    function clearingNodesValue() {
+        amountFridaysNode.innerText = '';
+        isLongMonthNode.innerText = 'Unknown';
+        sizeWeekNode.innerText = '';
+        weeksAmountNode.innerText = '';
+    }
 
-  // Processing the data that was entered
-  function submiting(e) {
-    e.preventDefault();
-    if (e.target[0].value) {
-      value = new Date(e.target[0].value);
-      date = new Date(value);
-      switch(e.submitter.value) {
-        case 'getFridays':
-          amountFridaysNode.innerText = getFridaysOfMonth(date).toString();
-          break;
-        case 'isLong':
-          isLongMonthNode.innerText = isMonthLong(date);
-          break;
-        case 'shortestWeek':
-          sizeWeekNode.innerText = shortestWeekDaysNumber(date);
-          break;
-        case 'fullWeeks':
-          weeksAmountNode.innerText = fullWeeksNumberInMonth(date);
-          break;
-        default: break;
-      }
-    } else alert('please, choose date');
-  }
+    // Processing the data that was entered
+    function submiting(e) {
+        e.preventDefault();
+        if (e.target[0].value) {
+            value = new Date(e.target[0].value);
+            date = new Date(value);
+            switch (e.submitter.value) {
+                case 'getFridays':
+                    amountFridaysNode.innerText = getFridaysOfMonth(date).toString();
+                    break;
+                case 'isLong':
+                    isLongMonthNode.innerText = isMonthLong(date);
+                    break;
+                case 'shortestWeek':
+                    sizeWeekNode.innerText = shortestWeekDaysNumber(date);
+                    break;
+                case 'fullWeeks':
+                    weeksAmountNode.innerText = fullWeeksNumberInMonth(date);
+                    break;
+                default: break;
+            }
+        } else alert('please, choose date');
+    }
 
-  // Getting current time or changing to selected
-  function gettingTime() {
-    inputZoneNode.value;
-  }
+    // Set current time or time changing to selected
+    function settingTime(timeZone) {
+        if (timeZone) {
+            clearInterval(timer);
+            timer = setInterval(() => {
+                hours = timeZone.getHours();
+                minutes = timeZone.getMinutes();
+                timeNode.innerText = `${hours < 10 ? `0${hours}` : hours}  : ${minutes < 10 ? `0${minutes}` : minutes}`;
+            }, 1000);
+        } else {
+            clearInterval(timer);
+            timer = setInterval(() => {
+                today = new Date();
+                hours = today.getHours();
+                minutes = today.getMinutes();
+                timeNode.innerText = `${hours < 10 ? `0${hours}` : hours}  : ${minutes < 10 ? `0${minutes}` : minutes}`;
+            }, 1000);
+        }
+    }
 
-  // gettingTime();
+    function gettingTime() {
+        // if another time zone was selected
+        if (inputZoneNode.value) {
+            timeDifference = parseInt(inputZoneNode.value, 10);
+            today = new Date();
+            timeZoneTime = calculationTimeZone(today, timeDifference);
+            settingTime(timeZoneTime);
+        } else settingTime(); // if chose current time
+    }
 
-  // Adding event listener
-  inputDateNode.addEventListener('change', clearingNodesValue);
-  formNode.addEventListener('submit', submiting);
-  inputZoneNode.addEventListener('change', gettingTime);
-})();
+    // setting time when page is opening at first time
+    settingTime();
+
+    // Adding event listener
+    inputDateNode.addEventListener('change', clearingNodesValue);
+    formNode.addEventListener('submit', submiting);
+    inputZoneNode.addEventListener('change', gettingTime);
+}());
--- homeworks/dmytro.shvets_Schweppes-JS/4-modules/time.js
+++ homeworks/dmytro.shvets_Schweppes-JS/4-modules/time.js
@@ -1,55 +1,67 @@
 // Determination how many fridays in selected month
-function getFridaysOfMonth (date) {
-  let fridays = [];
-  let day;
-  const month = date.getMonth();
-  const year = date.getFullYear();
-  const amountDays = new Date(year, month + 1, 0).getDate(); // '0' means last day previous month , '+1' means next month after selected
-  for (let i = 1; i <= amountDays; i++) {
-    // Determination data for every day in month
-    day = new Date(year, month, i).getDay();
-    // Cheking if each data is Friday 
-    if (day === 5) { // '5' means friday in Date object
-      fridays.push(new Date(year, month, i).getDate());
+function getFridaysOfMonth(date) {
+    const fridays = [];
+    let day;
+    const month = date.getMonth();
+    const year = date.getFullYear();
+    const amountDays = new Date(year, month + 1, 0).getDate(); // '0' means last day previous month
+    for (let i = 1; i <= amountDays; i++) {
+        // Determination data for every day in month
+        day = new Date(year, month, i).getDay();
+        // Cheking if each data is Friday
+        if (day === 5) { // '5' means friday in Date object
+            fridays.push(new Date(year, month, i).getDate());
+        }
     }
-  }
-  return fridays;
+    return fridays;
 }
 
-// Checking if selected month is long 
-function isMonthLong (date) {
-  const month = date.getMonth();
-  const year = date.getFullYear();
-  const amountDays = new Date(year, month + 1, 0).getDate();
-  return amountDays === 31;
+// Checking if selected month is long
+function isMonthLong(date) {
+    const month = date.getMonth();
+    const year = date.getFullYear();
+    const amountDays = new Date(year, month + 1, 0).getDate();
+    return amountDays === 31;
 }
 
 // Determination how many days have the shortest week in selected month
-function shortestWeekDaysNumber (date) {
-  const month = date.getMonth();
-  const year = date.getFullYear();
-  const lastDay = new Date(year, month + 1, 0).getDay();
-  const firstDay = new Date(year, month, 1).getDay();
-  // comparing position first day and last day of month
-  return lastDay < 6 - firstDay ? lastDay + 1 : 7 - firstDay;
+function shortestWeekDaysNumber(date) {
+    const month = date.getMonth();
+    const year = date.getFullYear();
+    const lastDay = new Date(year, month + 1, 0).getDay();
+    const firstDay = new Date(year, month, 1).getDay();
+    // comparing position first day and last day of month
+    return lastDay < 6 - firstDay ? lastDay + 1 : 7 - firstDay;
 }
 
 // Calculation how many full weeks in month
-function fullWeeksNumberInMonth (date) {
-  let mondays = [];
-  let amountWeeks = 0;
-  let day;
-  const month = date.getMonth();
-  const year = date.getFullYear();
-  const amountDays = new Date(year, month + 1, 0).getDate();
-  for (let i = 1; i <= amountDays; i++) {
-    day = new Date(year, month, i).getDay();
-    if (day === 0) { // '0' means monday in Date object
-      mondays.push(new Date(year, month, i).getDate());
+function fullWeeksNumberInMonth(date) {
+    const mondays = [];
+    let amountWeeks = 0;
+    let day;
+    const month = date.getMonth();
+    const year = date.getFullYear();
+    const amountDays = new Date(year, month + 1, 0).getDate();
+    for (let i = 1; i <= amountDays; i++) {
+        day = new Date(year, month, i).getDay();
+        if (day === 0) { // '0' means monday in Date object
+            mondays.push(new Date(year, month, i).getDate());
+        }
     }
-  }
-  mondays.forEach((monday) => monday + 6 <= amountDays ? amountWeeks++ : false);
-  return amountWeeks;
+    mondays.forEach((monday) => (monday + 6 <= amountDays ? amountWeeks++ : false));
+    return amountWeeks;
 }
 
-export { getFridaysOfMonth, isMonthLong, shortestWeekDaysNumber, fullWeeksNumberInMonth };
+function calculationTimeZone(date, hours) {
+    const resultDate = date;
+    resultDate.setHours((date.getUTCHours() + hours));
+    return resultDate;
+}
+
+export {
+    getFridaysOfMonth,
+    isMonthLong,
+    shortestWeekDaysNumber,
+    fullWeeksNumberInMonth,
+    calculationTimeZone,
+};
