a689c84f443ddf47b3a65650cdf2858cabf3eb58
Angel-88 <dasha.zagrebelna.08.01.02@gmail.com>
2020-11-13 17:00:01 +0200

    fix

--- homeworks/daria.zahrebelna_Angel-88/4-modules/script.js
+++ homeworks/daria.zahrebelna_Angel-88/4-modules/script.js
@@ -16,17 +16,21 @@ const buttonDayOfShortestWeek = document.querySelector('[data-button-day-of-shor
 const dayOfShortestWeek = document.querySelector('[data-day-of-shortest-week]');
 
 buttonGetMonthFridays.addEventListener('click', () => {
+    if (!date.value) return;
     fridaysInMonth.innerText = getFridaysOfMonth(new Date(date.value));
 });
 
 buttonIsMonthLong.addEventListener('click', () => {
+    if (!date.value) return;
     lengthInMonth.innerText = isMonthLong(new Date(date.value));
 });
 
 buttonDayOfShortestWeek.addEventListener('click', () => {
+    if (!date.value) return;
     dayOfShortestWeek.innerText = shortestWeekDaysNumber(new Date(date.value));
 });
 
 buttonFullWeeks.addEventListener('click', () => {
+    if (!date.value) return;
     fullWeeks.innerText = fullWeeksNumberInMonth(new Date(date.value));
 });
