f652b9c3b40d35d043bfb7592d8c613a4e29fb14
Illia <z3prodigy07@gmail.com>
2020-11-15 21:43:39 +0200

    fix 5

--- homeworks/Illia.Lototskyi_ILototskyi/4-modules/js/time.js
+++ homeworks/Illia.Lototskyi_ILototskyi/4-modules/js/time.js
@@ -1,60 +1,58 @@
-/* eslint-disable no-use-before-define */
-// eslint-disable-next-line no-unused-vars
 export function getFridays(date) {
-    const friday = [];
-    const monthData = daysOfMonth(date);
-    // eslint-disable-next-line no-undef
+    const dateInput = date;
+    const arrayFriday = [];
+    const monthData = new Date(dateInput.getFullYear(),
+        dateInput.getMonth() + 1, 0).getDate();
+
     for (let i = 1; i <= monthData; i++) {
-        // eslint-disable-next-line no-undef
-        const dateFridays = new Date(date.getFullYear(), date.getMonth(), i);
-        if (dateFridays.getDay() === 5) {
-            friday.push(dateFridays.getDate());
+        const currentData = new Date(dateInput.getFullYear(), dateInput.getMonth(), i);
+        if (currentData.getDay() === 5) {
+            arrayFriday.push(currentData.getDate());
         }
     }
-    return friday;
+    return arrayFriday;
 }
 
-function daysOfMonth(date) {
-    return 32 - new Date(date.getFullYear(), date.getMonth(), 32).getDate();
-}
-
-// eslint-disable-next-line no-unused-vars
 export function longMonth(date) {
-    // eslint-disable-next-line no-undef
-    return daysOfMonth(date) === 31;
+    return new Date(date.getFullYear(), date.getMonth(), 31).getMonth() === date.getMonth();
 }
 
-// eslint-disable-next-line no-unused-vars
 export function shortestWeek(date) {
-    // eslint-disable-next-line no-undef
-    const weekDaysNumber = daysOfMonth(date);
-    let weekDays = 0;
-    let daysNumber = 7;
-    for (let i = 1; i <= weekDaysNumber; i++) {
-        // eslint-disable-next-line no-undef
-        const dateFridays = new Date(date.getFullYear(), date.getMonth(), i);
-        // eslint-disable-next-line no-cond-assign
-        if (dateFridays.getDay() === 1 ? daysNumber > weekDays : daysNumber = weekDays) {
-            weekDays = 0;
+    const dateInput = date;
+    let array = [];
+    let minLength = 0;
+    const monthData = new Date(dateInput.getFullYear(),
+        dateInput.getMonth() + 1, 0).getDate();
+
+    for (let i = 1; i <= monthData; i++) {
+        const currentData = new Date(dateInput.getFullYear(), dateInput.getMonth(), i);
+        if (currentData.getDay() === 1) array = [];
+        array.push(currentData.getDay());
+        if (currentData.getDay() === 0 || currentData.getDate() === monthData) {
+            if (!minLength) minLength = array.length;
+            minLength = Math.min(minLength, array.length);
         }
-        weekDays++;
     }
-    return daysNumber;
+    return minLength;
 }
 
 export function fullWeeks(date) {
-    let fullWeeksInMonth = 0;
-    // eslint-disable-next-line no-shadow
-    const daysOfMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+    const dateInput = date;
+    let weeks = 1;
+    let day = -1;
+    const monthData = new Date(dateInput.getFullYear(),
+        dateInput.getMonth() + 1, 0).getDate();
 
-    for (let i = 1; i <= daysOfMonth; i++) {
-        const newData = new Date(date.getFullYear(), date.getMonth(), i);
-        if (newData.getDay() === 1) {
-            newData.setDate(newData.getDate() + 6);
-            if (newData.getMonth() === date.getMonth()) {
-                fullWeeksInMonth++;
-            }
+    const firstDay = new Date(dateInput.getFullYear(), dateInput.getMonth(), 1).getDay();
+    const lastDay = new Date(dateInput.getFullYear(), dateInput.getMonth() + 1, 0).getDay();
+    for (let i = 1; i <= monthData; i++) {
+        const newDay = new Date(dateInput.getFullYear(), dateInput.getMonth(), i).getDay();
+        if (day === 0 && newDay === 1) {
+            weeks += 1;
         }
+        day = newDay;
     }
-    return fullWeeksInMonth;
+    if (firstDay !== 1) weeks--;
+    if (lastDay !== 0) weeks--;
+    return weeks;
 }
