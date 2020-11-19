b96f89d39074821cec1acc30fda78c8314719c8e
rostyslav <ronyahavuka@gmail.com>
2020-11-19 13:16:12 +0200

    Fix after comment. Add task with *

--- homeworks/rostyslav.havuka_RostyslavHavuka/5-rxjs/main.js
+++ homeworks/rostyslav.havuka_RostyslavHavuka/5-rxjs/main.js
@@ -1,20 +1,64 @@
-const { fromEvent, animationFrameScheduler } = window.rxjs;
 const {
-    pairwise, map, distinctUntilChanged, throttleTime,
+    fromEvent,
+    animationFrameScheduler,
+} = window.rxjs;
+
+const {
+    pairwise,
+    map,
+    distinctUntilChanged,
+    throttleTime,
+    filter,
 } = window.rxjs.operators;
 
 const scroll = fromEvent(window, 'scroll');
-const header = document.querySelector('header');
+const firstHeader = document.querySelector('[data-first-header]');
+const secondHeader = document.querySelector('[data-second-header]');
+const thirdHeader = document.querySelector('[data-third-header]');
 
 const watchScroll = scroll.pipe(
-    throttleTime(150, animationFrameScheduler),
+    throttleTime(100, animationFrameScheduler),
     map(() => window.pageYOffset),
     pairwise(),
+    filter(([x]) => x < 1400),
     map(([y1, y2]) => (y2 < y1 ? 'Up' : 'Down')),
     distinctUntilChanged(),
 );
 
 watchScroll.subscribe((event) => {
-    if (event === 'Up') header.classList.add('header-logo');
-    if (event === 'Down') header.classList.remove('header-logo');
+    switch (event) {
+        case 'Up':
+            firstHeader.classList.add('header-first-logo');
+            break;
+        case 'Down':
+            secondHeader.classList.remove('header-second-logo');
+            firstHeader.classList.remove('header-first-logo');
+            break;
+        default: // do nothing
+            break;
+    }
+});
+
+const secondWatchScroll = scroll.pipe(
+    throttleTime(50, animationFrameScheduler),
+    map(() => window.pageYOffset),
+    pairwise(),
+    filter(([x]) => x > 1400),
+    map(([y1, y2]) => (y2 < y1 ? 'Up' : 'Down')),
+    distinctUntilChanged(),
+);
+
+secondWatchScroll.subscribe((event) => {
+    switch (event) {
+        case 'Up':
+            secondHeader.classList.remove('header-second-logo');
+            thirdHeader.classList.add('header-third-logo');
+            break;
+        case 'Down':
+            thirdHeader.classList.remove('header-third-logo');
+            secondHeader.classList.add('header-second-logo');
+            break;
+        default: // do nothing
+            break;
+    }
 });
