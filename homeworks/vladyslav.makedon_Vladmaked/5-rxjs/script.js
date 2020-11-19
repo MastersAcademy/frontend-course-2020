a05b854a64550531b2f696a7e4a328d387b9b217
Vladmaked <makedohska@ukr.net>
2020-11-20 00:57:59 +0200

    add change

--- homeworks/vladyslav.makedon_Vladmaked/5-rxjs/script.js
+++ homeworks/vladyslav.makedon_Vladmaked/5-rxjs/script.js
@@ -2,36 +2,61 @@ const { fromEvent } = window.rxjs;
 const {
     filter,
     map,
-    debounceTime,
+    throttleTime,
     pairwise,
 } = window.rxjs.operators;
 
-const headerNode = document.querySelector('[data-header]');
+const defaultHeaderNode = document.querySelector('[data-header]');
+const additionalHeaderNode = document.querySelector('[data-header-with-btn]');
 const headerDescriptionNode = document.querySelector('[data-header-description]');
+const additionalHeaderDescriptionLogoNode = document.querySelector('[data-additional-header-description-logo]');
 const btnNode = document.querySelector('[data-btn]');
-const copyBtn = btnNode.cloneNode(true);
 const btnPosition = btnNode.getBoundingClientRect().top + window.pageYOffset;
 
 fromEvent(window, 'scroll')
     .pipe(
-        debounceTime(50),
+        throttleTime(150),
         map(() => window.pageYOffset),
         pairwise(),
-        map((pageYOffset) => pageYOffset[1] - pageYOffset[0]),
-        filter((pageYOffset) => Math.abs(pageYOffset) >= 50),
-        map((pageYOffset) => pageYOffset > 0),
-    )
-    .subscribe((isScrolledToBottom) => {
-        if (window.pageYOffset >= btnPosition) {
-            headerDescriptionNode.innerText = 'get an amazing discount';
-            headerNode.appendChild(copyBtn);
-            headerNode.className = 'header visible';
-            headerDescriptionNode.innerText = isScrolledToBottom ? 'get an amazing discount' : 'Logo';
-        } else {
-            headerDescriptionNode.innerText = 'Logo';
-            if (headerNode.contains(copyBtn)) {
-                headerNode.removeChild(copyBtn);
+        map((scrolledHeight) => scrolledHeight[1] - scrolledHeight[0]),
+        filter((scrolledHeight) => Math.abs(scrolledHeight) >= 50),
+        map((scrolledHeight) => {
+            if (scrolledHeight > 0 && window.pageYOffset < btnPosition) {
+                return 'defaultHeaderHidden';
             }
-            headerNode.className = isScrolledToBottom ? 'header hidden' : 'header visible';
+            if (scrolledHeight < 0 && window.pageYOffset < btnPosition) {
+                return 'defaultHeaderVisible';
+            }
+            if (scrolledHeight > 0 && window.pageYOffset >= btnPosition) {
+                return 'additionalHeaderVisible';
+            }
+            if (scrolledHeight < 0 && window.pageYOffset >= btnPosition) {
+                return 'additionalHeaderDescriptionVisible';
+            }
+            return '';
+        }),
+    )
+    .subscribe((evt) => {
+        switch (evt) {
+            case 'defaultHeaderHidden':
+                defaultHeaderNode.className = 'header hidden';
+                additionalHeaderNode.className = 'header hidden';
+                break;
+            case 'defaultHeaderVisible':
+                defaultHeaderNode.className = 'header visible';
+                additionalHeaderNode.className = 'header hidden';
+                break;
+            case 'additionalHeaderVisible':
+                defaultHeaderNode.className = 'header hidden';
+                additionalHeaderNode.className = 'header visible';
+                headerDescriptionNode.className = 'header__description visibleText';
+                additionalHeaderDescriptionLogoNode.className = 'header__description hiddenText';
+                break;
+            case 'additionalHeaderDescriptionVisible':
+                additionalHeaderDescriptionLogoNode.className = 'header__description visibleText';
+                headerDescriptionNode.className = 'header__description hiddenText';
+                break;
+            default:
+                break;
         }
     });
