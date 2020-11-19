b958edf091073c55eb7289517a7851a22513e034
rostyslav <ronyahavuka@gmail.com>
2020-11-19 18:02:58 +0200

    another Fix

--- homeworks/rostyslav.havuka_RostyslavHavuka/5-rxjs/main.js
+++ homeworks/rostyslav.havuka_RostyslavHavuka/5-rxjs/main.js
@@ -18,48 +18,59 @@
     const thirdHeader = document.querySelector('[data-third-header]');
     const button = document.querySelector('[data-button]');
 
-    const watchScroll = scroll.pipe(
-        throttleTime(100, animationFrameScheduler),
-        map(() => window.pageYOffset),
-        pairwise(),
-        filter(([x]) => x < button.offsetTop),
-        map(([y1, y2]) => (y2 < y1 ? 'Up' : 'Down')),
-        distinctUntilChanged(),
-    );
+    function watchScroll() {
+        return scroll.pipe(
+            throttleTime(100, animationFrameScheduler),
+            map(() => window.pageYOffset),
+            pairwise(),
+            filter(([x]) => x < button.offsetTop),
+            map(([y1, y2]) => (y2 < y1 ? 'Up' : 'Down')),
+            distinctUntilChanged(),
+        );
+    }
 
-    watchScroll.subscribe((event) => {
+    watchScroll().subscribe((event) => {
         switch (event) {
             case 'Up':
+                thirdHeader.classList.remove('header-third-logo');
+                secondHeader.classList.remove('header-second-logo');
                 firstHeader.classList.add('header-first-logo');
+                console.log('up before button');
                 break;
             case 'Down':
                 thirdHeader.classList.remove('header-third-logo');
                 secondHeader.classList.remove('header-second-logo');
                 firstHeader.classList.remove('header-first-logo');
+                console.log('down before button');
                 break;
             default: // do nothing
                 break;
         }
     });
 
-    const secondWatchScroll = scroll.pipe(
-        throttleTime(400, animationFrameScheduler),
-        map(() => window.pageYOffset),
-        pairwise(),
-        filter(([x]) => x > button.offsetTop),
-        map(([y1, y2]) => (y2 < y1 ? 'Up' : 'Down')),
-        distinctUntilChanged(),
-    );
-
-    secondWatchScroll.subscribe((event) => {
+    function secondWatchScroll() {
+        return scroll.pipe(
+            throttleTime(100, animationFrameScheduler),
+            map(() => window.pageYOffset),
+            pairwise(),
+            filter(([x]) => x > button.offsetTop),
+            map(([y1, y2]) => (y2 < y1 ? 'Up' : 'Down')),
+            distinctUntilChanged(),
+        );
+    }
+    secondWatchScroll().subscribe((event) => {
         switch (event) {
             case 'Up':
+                firstHeader.classList.remove('header-first-logo');
                 secondHeader.classList.remove('header-second-logo');
                 thirdHeader.classList.add('header-third-logo');
+                console.log('up after button');
                 break;
             case 'Down':
+                firstHeader.classList.remove('header-first-logo');
                 thirdHeader.classList.remove('header-third-logo');
                 secondHeader.classList.add('header-second-logo');
+                console.log('down after button');
                 break;
             default: // do nothing
                 break;
