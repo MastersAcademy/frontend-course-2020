5163cbc7626e62a8966458adffae3285a304ccc9
Volodymyr Ustilkin <volodymyr.ustilkin@gmail.com>
2020-11-19 11:27:41 +0200

    hw5 fixed

--- homeworks/volodymyr.ustilkin_VolodymyrUstilkin/5-rxjs/index.js
+++ homeworks/volodymyr.ustilkin_VolodymyrUstilkin/5-rxjs/index.js
@@ -6,11 +6,8 @@
         tap,
         filter,
         map,
-        // throttleTime,
-        // distinctUntilChanged,
         pairwise,
         debounceTime,
-        // switchMap,
     } = window.rxjs.operators;
 
     const headerShowClass = 'header-show';
@@ -18,6 +15,18 @@
 
     const headerEl = document.querySelector('[data-header]');
     const contentEl = document.querySelector('[data-main-container]');
+    const buttonEl = document.querySelector('[data-button]');
+
+    function setHeaderButton() {
+        headerEl.innerHTML = '<div class="header-inner">Get an amazing<br>discount</div>'
+            + '<button class="buy-button header-inner" type="button">BUY NOW</button>';
+        console.log('setted button');
+    }
+
+    function setHeaderLogo() {
+        headerEl.innerHTML = '<div class="header-inner">LOGO</div>';
+        console.log('setted logo');
+    }
 
     function showHeader() {
         headerEl.classList.remove(headerHideClass);
@@ -29,29 +38,37 @@
         headerEl.classList.add(headerHideClass);
     }
 
-    // function resetHeader() {
-    //     headerEl.classList.remove(headerHideClass);
-    //     headerEl.classList.remove(headerShowClass);
-    // }
+    function resetHeader() {
+        headerEl.classList.remove(headerHideClass);
+        headerEl.classList.remove(headerShowClass);
+    }
 
-    fromEvent(contentEl, 'scroll')
+    fromEvent(contentEl, 'scroll') // scroll up (show header)
         .pipe(
             map((e) => e.currentTarget.scrollTop),
-            debounceTime(500),
+            debounceTime(100),
             pairwise(),
-            filter((e) => e[0] - e[1] > 50 && e[1] > 1),
-            tap((e) => console.log(e)),
+            filter((e) => ((e[0] - e[1] > 50 && e[1] > 1) || (buttonEl.offsetTop < e[1]))),
+            tap((e) => (buttonEl.offsetTop < e[1] ? setHeaderButton() : setHeaderLogo())),
         )
         .subscribe(() => showHeader());
 
+    fromEvent(contentEl, 'scroll') // scroll down (hide header)
+        .pipe(
+            map((e) => e.currentTarget.scrollTop),
+            debounceTime(100),
+            filter((e) => headerEl.classList.contains(headerShowClass) && buttonEl.offsetTop > e),
+            pairwise(),
+            filter((e) => e[1] - e[0] > 50),
+        )
+        .subscribe(() => hideHeader());
+
     fromEvent(contentEl, 'scroll')
         .pipe(
             map((e) => e.currentTarget.scrollTop),
-            debounceTime(500),
-            filter(() => headerEl.classList.contains(headerShowClass)),
-            pairwise(),
-            filter((e) => e[1] - e[0] > 50),
+            debounceTime(100),
+            filter((e) => e <= 1),
             tap((e) => console.log(e)),
         )
-        .subscribe(() => hideHeader());
+        .subscribe(() => resetHeader());
 }());
