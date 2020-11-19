9f658bbc749a4fd1a2754133d66c330bbadb384b
Nataliia Koval <hakunata@gmail.com>
2020-11-19 23:04:11 +0200

    fix

--- homeworks/nataliia.koval_NataliyaKoval/5-rxjs/script.js
+++ homeworks/nataliia.koval_NataliyaKoval/5-rxjs/script.js
@@ -1,6 +1,10 @@
 const { fromEvent } = window.rxjs;
 const {
-    map, throttleTime, pairwise, distinctUntilChanged, filter,
+    map,
+    throttleTime,
+    pairwise,
+    distinctUntilChanged,
+    filter,
 } = window.rxjs.operators;
 
 const scroll = fromEvent(document, 'scroll');
@@ -8,13 +12,16 @@ const header = document.querySelector('[data-header]');
 
 scroll.pipe(
     throttleTime(500),
-    map(() => window.pageYOffset),
+    map(() => window.scrollY),
     pairwise(),
     filter(([a, b]) => (a - b) > 50 || (b - a) > 50),
     map(([a, b]) => (a < b ? 'down' : 'up')),
     distinctUntilChanged(),
 )
     .subscribe((event) => {
-        if (event === 'up') header.classList.remove('hide');
-        else header.classList.add('hide');
+        if (event === 'up') {
+            header.classList.remove('hide');
+        } else {
+            header.classList.add('hide');
+        }
     });
