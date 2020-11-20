e2f7c042c05ef93bea0b7da33c154128fd6fc3aa
spavel-gh <spavel.mail@gmail.com>
2020-11-21 00:51:38 +0200

    Working model with RxJS, no 50px scroll tracking.

--- homeworks/sokurenko.pavel_spavel_gh/5-rxjs/index.js
+++ homeworks/sokurenko.pavel_spavel_gh/5-rxjs/index.js
@@ -1,49 +1,79 @@
-const mainNode = document.querySelector('[data-main]');
-const headerNode = document.querySelector('[data-heaader]');
+// const mainNode = document.querySelector('[data-main]');
+// const headerTTTNode = document.querySelector('[data-heaader]');
+const headerNode = document.querySelector('[data-heaader-content]');
 const scrollNode = document.querySelector('[data-scroll]');
 
-headerNode.style.width = `${scrollNode.clientWidth}px`;
-mainNode.style.paddingTop = `${headerNode.offsetHeight + 1}px`;
+// headerNode.style.width = `${scrollNode.clientWidth}px`;
+// mainNode.style.paddingTop = `${headerNode.offsetHeight + 1}px`;
 
 // Да уж, та еще задачка, ехеххе 😵
 function doLittleMagic(action) {
     switch (action) {
         case 'close':
+            headerNode.style.top = '0px';
             headerNode.style.top = `${(headerNode.offsetHeight - 2) * -1}px`;
             // todo '- 2' -> '+ n'                              ☝
             // ой на что же готов человек без интернета...
             break;
         case 'open':
-            headerNode.style.top = `${scrollNode.offsetTop}px`;
+            headerNode.style.top = '0px';
             break;
         default:
-            headerNode.style.top = `${scrollNode.offsetTop}px`;
             break;
     }
 } doLittleMagic();
+// headerTTTNode.classList.remove('header-content-sticky-top0');
+// headerTTTNode.classList.add('header-content-sticky-top0');
 
-/**
- * model without internet and rxjs
- *
- */
-
+//
 // scrollNode.addEventListener('scroll', (e) => {
 //     console.log(scrollNode.scrollTop);
 //     // console.log(scrollNode.scrollTop);
 // });
+//
 
-// const { fromEvent } = window.rxjs;
-// const { map } = window.rxjs.operators;
+function whereScroll(oldY, newY) {
+    return (newY > oldY ? 'down' : 'up');
+}
 
-// const subscription = fromEvent(scrollNode, 'scroll');
+// 2
+// 2
+// 2
+// 2
 
-// const subscription = myObservable.subscribe((event) => console.log(event));
+const { fromEvent } = window.rxjs;
+const {
+    throttleTime, pairwise, distinctUntilChanged, map, /* tap */
+} = window.rxjs.operators;
 
-// const subscription = myObservable.subscribe({
-//     // on successful emissions
-//     next: (event) => console.log(event),
-//     // on errors
-//     error: (error) => console.log(error),
-//     // called once on completion
-//     complete: () => console.log('complete!')
-//   });
+const scroll = fromEvent(scrollNode, 'scroll');
+
+// let scrollPointer = scrollNode.scrollTop;
+
+// let wP = "up";
+// let pointerY = 0;
+// let watch = false;
+
+const ddd = scroll.pipe(
+    throttleTime(80),
+    map(() => scrollNode.scrollTop),
+    distinctUntilChanged(),
+    pairwise(),
+
+    // tap(([oldY, newY]) => {
+    //     if (wP !== whereScroll(oldY, newY)) { //если новое направление
+    //         if (watch === false) { // если мы еще не отслеживаем
+    //             watch = true; // начинаем отслеживать
+    //             wP = newY;
+    //         }
+    //     }
+    // }),
+
+    map(([oldY, newY]) => whereScroll(oldY, newY)),
+    // distinctUntilChanged(),
+);
+
+ddd.subscribe((event) => {
+    if (event === 'up') doLittleMagic('open');
+    if (event === 'down') doLittleMagic('close');
+});
