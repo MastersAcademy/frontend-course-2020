1fccac0dd5a042ff6c5b33d860d33dff04fd5e3c
MatviienkoOleh <matviienkooleh@gmail.com>
2020-11-21 00:45:22 +0200

    commit

--- homeworks/oleh.matviienko_MatviienkoOleh/5-rxjs/main.js
+++ homeworks/oleh.matviienko_MatviienkoOleh/5-rxjs/main.js
@@ -11,12 +11,14 @@ fromEvent(window, 'scroll').pipe(
     throttleTime(300),
     pairwise(),
     map(([a, b]) => {
+        let direction;
         if ( a > b ) {
-            if ((a - b) >= 50) return 'Up';
+            if ((a - b) >= 50) direction = 'Up';
         }
         if ( a < b ) {
-            if ((b - a) >= 50) return 'Down';
+            if ((b - a) >= 50) direction = 'Down';
         }
+        return direction;
     }),
 ).subscribe((event) => {
     switch (event) {
