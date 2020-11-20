0d99c26659f35f8aa83577d4eddce0f008f9eae8
MatviienkoOleh <matviienkooleh@gmail.com>
2020-11-21 00:03:36 +0200

    commit

--- homeworks/oleh.matviienko_MatviienkoOleh/5-rxjs/main.js
+++ homeworks/oleh.matviienko_MatviienkoOleh/5-rxjs/main.js
@@ -10,20 +10,18 @@ fromEvent(window, 'scroll').pipe(
     throttleTime(300),
     pairwise(),
     map(([a,b]) => {
-        if(a > b) {
+        if (a > b) {
             if ((a - b) >= 50) return 'Up';
         }
-        if(a < b) {
+        if (a < b) {
             if ((b - a) >= 50) return 'Down';
         }
     }),
-).subscribe((event) => {
-    switch (event) {
-        case 'Up':
-            header.classList.add('header-show');
-            break;
-        case 'Down':
-            header.classList.remove('header-show');
-            break;
+).subscribe((e) => {
+    if (e == 'Up') {
+        header.classList.add('header-show');
+    }
+    if (e == 'Down') {
+        header.classList.remove('header-show');
     }
 });
