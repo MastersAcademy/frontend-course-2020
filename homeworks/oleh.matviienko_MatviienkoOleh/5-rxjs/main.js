8ff7108c8a93fac3002bd9867eab9ef33120b9f9
MatviienkoOleh <matviienkooleh@gmail.com>
2020-11-20 23:43:39 +0200

    commit

---
+++ homeworks/oleh.matviienko_MatviienkoOleh/5-rxjs/main.js
@@ -0,0 +1,29 @@
+const { fromEvent } = window.rxjs;
+const { map,
+    throttleTime,
+    pairwise,
+} = window.rxjs.operators;
+
+const header = document.getElementById('header');
+fromEvent(window, 'scroll').pipe(
+    map(() => window.pageYOffset),
+    throttleTime(300),
+    pairwise(),
+    map(([a,b]) => {
+        if ( a > b ) {
+            if ((a - b) >= 50) return 'Up';
+        }
+        if ( a < b ) {
+            if ((b - a) >= 50) return 'Down';
+        }
+    }),
+).subscribe((event) => {
+    switch (event) {
+        case 'Up':
+            header.classList.add('header-show');
+            break;
+        case 'Down':
+            header.classList.remove('header-show');
+            break;
+    }
+});
