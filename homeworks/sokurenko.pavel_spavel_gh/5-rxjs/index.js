8f879b4c1c347eb976bd1fd2213cc0791cdf2385
spavel-gh <spavel.mail@gmail.com>
2020-11-22 00:03:55 +0200

    working prototype

--- homeworks/sokurenko.pavel_spavel_gh/5-rxjs/index.js
+++ homeworks/sokurenko.pavel_spavel_gh/5-rxjs/index.js
@@ -1,10 +1,17 @@
+// header variables
 const headerNode = document.querySelector('[data-heaader-content]');
+const headerLogoNode = document.querySelector('[data-header-logo]');
+const headerTextNode = document.querySelector('[data-header-text]');
+const headerButtonNode = document.querySelector('[data-header-button]');
+let headerContentStatus = 0;
+
 const scrollNode = document.querySelector('[data-scroll]');
 
 const offerWrapperNode = document.querySelector('[data-offer-wrapper]');
-const offerNode = document.querySelector('[data-offer]');
-const offerTextContentNode = document.querySelector('[data-offer-text]');
-const yOfferConst = offerWrapperNode.offsetTop;
+// const offerNode = document.querySelector('[data-offer]');
+// const offerTextContentNode = document.querySelector('[data-offer-text]');
+
+const yOfferConst = offerWrapperNode.offsetTop; // <---------- до этого момента по старому
 
 const wayLength = 50;
 let wayPointer = 'up';
@@ -13,29 +20,60 @@ let watch = false;
 
 const { fromEvent } = window.rxjs;
 const {
-    throttleTime, pairwise, distinctUntilChanged, map, tap, debounceTime,
+    throttleTime, pairwise, distinctUntilChanged, map, tap, debounceTime, filter,
 } = window.rxjs.operators;
 
 const scroll = fromEvent(scrollNode, 'scroll');
 
-function offerContent(toHide) {
-    if (toHide) offerTextContentNode.classList.add('hidden');
-    else offerTextContentNode.classList.remove('hidden');
-}
+// function offerContent(toHide) {
+//     if (toHide) offerTextContentNode.classList.add('hidden');
+//     else offerTextContentNode.classList.remove('hidden');
+// }
 
-function offerMover(offerPosition) {
-    if (offerPosition === 'top') {
-        offerNode.style.top = '0px';
-    }
-    if (offerPosition === 'second') {
-        offerNode.style.top = `${(headerNode.offsetHeight)}px`;
-    }
-}
+// function offerMover(offerPosition) {
+//     if (offerPosition === 'top') {
+//         offerNode.style.top = '0px';
+//     }
+//     if (offerPosition === 'second') {
+//         offerNode.style.top = `${(headerNode.offsetHeight)}px`;
+//     }
+// }
 
 function headerHideShow(action) {
     headerNode.style.top = '0px';
     if (action === 'hide') {
         headerNode.style.top = `${(headerNode.offsetHeight + 2) * -1}px`;
+    }
+}
+
+function changeHeaderContent(yOld, yNew) {
+    let id;
+    // (yNew - (yOld - yNew))
+    if (yNew < yOfferConst) id = 0;
+    if (yNew > yOfferConst && yOld > yNew) id = 1;
+    if (yNew > yOfferConst && yOld < yNew) id = 2;
+
+    console.log(id, yOld, yNew);
+
+    if (id === 0 && id !== headerContentStatus) {
+        headerLogoNode.classList.remove('hidden');
+        headerTextNode.classList.add('hidden');
+        headerButtonNode.classList.add('hidden');
+        headerContentStatus = 0;
+    }
+    if (id === 1 && id !== headerContentStatus) {
+        headerLogoNode.classList.remove('hidden');
+        headerTextNode.classList.add('hidden');
+        headerButtonNode.classList.remove('hidden');
+        headerContentStatus = 1;
+        headerHideShow('show');
+    }
+    if (id === 2 && id !== headerContentStatus) {
+        headerLogoNode.classList.add('hidden');
+        headerTextNode.classList.remove('hidden');
+        headerButtonNode.classList.remove('hidden');
+        headerContentStatus = 2;
+        headerHideShow('show');
     }
 }
 
@@ -60,7 +98,7 @@ opener.subscribe((event) => {
     if (event) {
         setTrackingStatus(false);
         wayPointer = 'up';
-        headerHideShow('open');
+        headerHideShow('show');
         console.log('Yeah!');
     }
 });
@@ -69,6 +107,7 @@ const scrollStream = scroll.pipe(
     throttleTime(80),
     map(() => scrollNode.scrollTop),
     map((y) => Math.round(y)),
+    filter((y) => y < yOfferConst),
     distinctUntilChanged(),
     pairwise(),
 
@@ -90,20 +129,36 @@ const scrollStream = scroll.pipe(
             wayPointer = getScrollDirection(yOld, yNew);
             headerHideShow(wayPointer === 'down' ? 'hide' : 'show');
             setTrackingStatus(false);
+            changeHeaderContent(yOld, yNew);
 
-            // here offer follows header
-            if (yNew > yOfferConst) offerMover(wayPointer === 'down' ? 'top' : 'second');
+            // // here offer follows header
+            // if (yNew > yOfferConst) offerMover(wayPointer === 'down' ? 'top' : 'second');
         }
     }),
+    tap(([yOld, yNew]) => changeHeaderContent(yOld, yNew)),
 
-    tap(([yOld, yNew]) => {
-        // the offer must be on top if we are higher this
-        offerContent((yNew - (yOld - yNew)) < yOfferConst);
-        if ((yNew - (yOld - yNew)) < yOfferConst) offerMover('top');
-    }),
+    // tap(([yOld, yNew]) => {
+    //     // the offer must be on top if we are higher this
+    //     offerContent((yNew - (yOld - yNew)) < yOfferConst);
+    //     if ((yNew - (yOld - yNew)) < yOfferConst) offerMover('top');
+    // }),
 
-    distinctUntilChanged(),
     tap((([yOld, yNew]) => console.log(`yOld : ${yOld}\t\tнапр : ${getScrollDirection(yOld, yNew)}\nyNew : ${yNew}    \tотслеж : ${watch}`))),
 );
 
 scrollStream.subscribe();
+
+const dddd = scroll.pipe(
+    throttleTime(80),
+    map(() => scrollNode.scrollTop),
+    map((y) => Math.round(y)),
+    filter((y) => y > yOfferConst),
+    distinctUntilChanged(),
+    pairwise(),
+    tap(([yOld, yNew]) => changeHeaderContent(yOld, yNew)),
+
+);
+
+dddd.subscribe((event) => {
+    console.log(event);
+});
