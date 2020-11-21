f324685a153bcdc3d1b94200dad8a26805b5973d
Andrey Kushch <droni2442@gmail.com>
2020-11-21 19:04:30 +0200

    linter fixes

--- homeworks/andrey.kushch_andrey241991/5-rxjs/index.js
+++ homeworks/andrey.kushch_andrey241991/5-rxjs/index.js
@@ -1,34 +1,31 @@
-(function () {
-    const { Observable, fromEvent } = window.rxjs;
+const { Observable, fromEvent } = window.rxjs;
 
-    const headerElem = document.querySelector('[data-header]');
+const headerElem = document.querySelector("[data-header]");
 
-    let scrollPossition = 0;
-    let currentScrollPossition = 0;
-    fromEvent(window, 'scroll').subscribe(() => {
-        currentScrollPossition = window.scrollY;
-    });
+let scrollPossition = 0;
+let currentScrollPossition = 0;
+fromEvent(window, "scroll").subscribe(() => {
+    currentScrollPossition = window.scrollY;
+});
 
-    const stream = new Observable((observer) => {
-        setInterval(() => {
-            observer.next(currentScrollPossition);
-        }, 400);
-    });
+const stream = new Observable((observer) => {
+    setInterval(() => {
+        observer.next(currentScrollPossition);
+    }, 400);
+});
 
-    function toggleHeader(currentSP) {
-        if (scrollPossition - currentSP <= -50) {
-            scrollPossition = currentSP;
-            headerElem.classList.add('header__not-active');
-            headerElem.classList.remove('header__active');
-        }
-
-        if (scrollPossition - currentSP >= 50) {
-            scrollPossition = currentSP;
-            headerElem.classList.add('header__active');
-            headerElem.classList.remove('header__not-active');
-        }
+function toggleHeader(currentSP) {
+    if (scrollPossition - currentSP <= -50) {
+        scrollPossition = currentSP;
+        headerElem.classList.add("header__not-active");
+        headerElem.classList.remove("header__active");
     }
 
-    stream.subscribe((val) => toggleHeader(val));
+    if (scrollPossition - currentSP >= 50) {
+        scrollPossition = currentSP;
+        headerElem.classList.add("header__active");
+        headerElem.classList.remove("header__not-active");
+    }
+}
 
-})();
+stream.subscribe((val) => toggleHeader(val));
