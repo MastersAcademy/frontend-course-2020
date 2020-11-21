04922e47c4c6afb1dc21a261500aa48786fcc4ae
N_Yarovenko <mykolayarovenko@gmail.com>
2020-11-21 14:57:18 +0200

    fixed

--- homeworks/mykola.yarovenko_MykYarovenko36/5-rxjs/index.js
+++ homeworks/mykola.yarovenko_MykYarovenko36/5-rxjs/index.js
@@ -7,14 +7,14 @@ const wrapperScroll$ = fromEvent(document.querySelector('[data-wrapper]'), 'scro
 const headerEl = document.querySelector('.wrapper_header');
 const saleBtnPosition = document.querySelector('.wpapper-sale_btn').offsetTop;
 const headerSaleContent = (pairScrollValue, btnPosition) => {
-    if (pairScrollValue[1] > btnPosition + 100) {
+    if (pairScrollValue[1] > btnPosition || pairScrollValue[1] > btnPosition - 20) {
         if (pairScrollValue[0] > pairScrollValue[1]) {
             headerEl.innerHTML = `
             <p class='header_logo'>Logo</p>
             <button class='header-sale_btn sale_btn'>BUY NOW</button>`;
         } else {
             headerEl.innerHTML = `
-            <p class='header_logo'>Get an amzing discount</p>
+            <p class='header_logo'>Get an amazing discount</p>
             <button class='header-sale_btn sale_btn'>BUY NOW</button>`;
         }
     } else {
@@ -22,9 +22,10 @@ const headerSaleContent = (pairScrollValue, btnPosition) => {
         <p class='header_logo'>Logo</p>`;
     }
 };
+
 wrapperScroll$.pipe(
     map((event) => event.target.scrollTop),
-    filter((val) => val % 50 === 0),
+    filter((val) => val > 50),
     pairwise(),
 ).subscribe(
     (val) => {
