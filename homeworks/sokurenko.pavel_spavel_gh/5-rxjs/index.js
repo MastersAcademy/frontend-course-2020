fe9cf0425654238452f1b45d1b81ec1e04ca3419
spavel-gh <spavel.mail@gmail.com>
2020-11-21 07:16:26 +0200

    cleaning up code

--- homeworks/sokurenko.pavel_spavel_gh/5-rxjs/index.js
+++ homeworks/sokurenko.pavel_spavel_gh/5-rxjs/index.js
@@ -1,50 +1,32 @@
-// const mainNode = document.querySelector('[data-main]');
-// const headerTTTNode = document.querySelector('[data-heaader]');
 const headerNode = document.querySelector('[data-heaader-content]');
 const scrollNode = document.querySelector('[data-scroll]');
 
-// headerNode.style.width = `${scrollNode.clientWidth}px`;
-// mainNode.style.paddingTop = `${headerNode.offsetHeight + 1}px`;
-
-// Да уж, та еще задачка, ехеххе 😵
-function doLittleMagic(action) {
+function headerWork(action) {
     switch (action) {
         case 'close':
             headerNode.style.top = '0px';
-            headerNode.style.top = `${(headerNode.offsetHeight - 2) * -1}px`;
-            // todo '- 2' -> '+ n'                              ☝
-            // ой на что же готов человек без интернета...
+            headerNode.style.top = `${(headerNode.offsetHeight + 2) * -1}px`;
             break;
         case 'open':
             headerNode.style.top = '0px';
             break;
-        default:
-            break;
+        default: break;
     }
-} doLittleMagic();
-// headerTTTNode.classList.remove('header-content-sticky-top0');
-// headerTTTNode.classList.add('header-content-sticky-top0');
+}
 
-//
-// scrollNode.addEventListener('scroll', (e) => {
-//     console.log(scrollNode.scrollTop);
-//     // console.log(scrollNode.scrollTop);
-// });
-//
-
-function whereScroll(oldY, newY) {
-    return (newY > oldY ? 'down' : 'up');
+function whereScroll(yOld, yNew) {
+    return (yNew > yOld ? 'down' : 'up');
 }
 
 const wayLength = 50;
 let wayPointer = 'up';
 let yPointer = 0;
 let watch = false;
-function makeTrackingStatus(command, newYPointer) {
+function makeTrackingStatus(command, yNewPointer) {
     switch (command) {
         case true:
             watch = true;
-            yPointer = newYPointer;
+            yPointer = yNewPointer;
             break;
         case false:
             watch = false;
@@ -70,7 +52,7 @@ opener.subscribe((event) => {
     if (event) {
         makeTrackingStatus(false);
         wayPointer = 'up';
-        doLittleMagic('open');
+        headerWork('open');
         console.log('Yeah!');
     }
 });
@@ -82,58 +64,45 @@ const scrollStream = scroll.pipe(
     distinctUntilChanged(),
     pairwise(),
 
-    tap(([oldY, newY]) => { // тригер начала отслеживания
-        if (wayPointer !== whereScroll(oldY, newY)) { // если новое направление
-        // ? направление будет новым, пока не пройдём по нему 50пкс
-            if (watch === false) { // если еще не отслеживаем
-                makeTrackingStatus(true, newY);
+    tap(([yOld, yNew]) => { // start tracking trigger
+        if (wayPointer !== whereScroll(yOld, yNew)) {
+            if (watch === false) {
+                makeTrackingStatus(true, yNew);
             }
         }
     }),
 
-    tap((oldY, newY) => { // прекратить отслеживать если движимся как раньше
+    tap((yOld, yNew) => { // stop tracking trigger
         if (watch === true) {
-            if (wayPointer === 'down') { // если базовое направление вниз
-                if (newY > yPointer) { // если мы ниже точки
+            if (wayPointer === 'down') {
+                if (yNew > yPointer) {
                     makeTrackingStatus(false);
                 }
-            } else if (wayPointer === 'up') { // если базовое направление вверх
-                if (newY < yPointer) { // мы выше точки
+            } else if (wayPointer === 'up') {
+                if (yNew < yPointer) {
                     makeTrackingStatus(false);
                 }
             }
         }
     }),
 
-    tap((oldY, newY) => {
-        if (oldY > newY) {
-            if (newY <= (oldY - newY)) {
-                makeTrackingStatus(false);
-                wayPointer = 'up';
-                doLittleMagic('open');
-                console.log('yeah!');
-            }
-        }
-    }),
-
-    tap(([oldY, newY]) => {
+    tap(([yOld, yNew]) => { // stop tracking trigger  and release header
         if (watch === true) {
-            if (Math.abs(yPointer - newY) > wayLength) { // если прошли путь
-                wayPointer = whereScroll(oldY, newY);
-                // открываем/закрываем хеадер
-                doLittleMagic(wayPointer === 'down' ? 'close' : 'open');
+            if (Math.abs(yPointer - yNew) > wayLength) {
+                wayPointer = whereScroll(yOld, yNew);
+                headerWork(wayPointer === 'down' ? 'close' : 'open');
                 makeTrackingStatus(false);
             }
         }
     }),
 
     distinctUntilChanged(),
-    tap((([oldY, newY]) => console.log(`oldY : ${oldY}\t\tнапр : ${whereScroll(oldY, newY)}\nnewY : ${newY}    \tотслеж : ${watch}`))),
-    map(([oldY, newY]) => whereScroll(oldY, newY)),
+    tap((([yOld, yNew]) => console.log(`yOld : ${yOld}\t\tнапр : ${whereScroll(yOld, yNew)}\nyNew : ${yNew}    \tотслеж : ${watch}`))),
+    // map(([yOld, yNew]) => whereScroll(yOld, yNew)),
 );
 
 scrollStream.subscribe((/* event */) => {
     // todo: do pretty code
-    // if (event === 'up') doLittleMagic('open');
-    // if (event === 'down') doLittleMagic('close');
+    // if (event === 'up') headerWork('open');
+    // if (event === 'down') headerWork('close');
 });
