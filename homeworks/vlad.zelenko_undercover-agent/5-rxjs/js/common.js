0cac25249fb4fcf915e0153e42f6613bfae25f74
VladZelenko <Vlad6555z@gmail.com>
2020-11-22 00:06:30 +0200

    added filter but no hide mune to scroll down

--- homeworks/vlad.zelenko_undercover-agent/5-rxjs/js/common.js
+++ homeworks/vlad.zelenko_undercover-agent/5-rxjs/js/common.js
@@ -1,13 +1,16 @@
 const { fromEvent } = window.rxjs;
-const { map, pairwise } = window.rxjs.operators;
+const { map, pairwise, filter } = window.rxjs.operators;
 const header = document.querySelector('[data-header]');
 const scrollEvent = fromEvent(window, 'scroll');
+const scrollStep = 50;
 
 scrollEvent.pipe(
     map(() => window.pageYOffset),
     pairwise(),
-).subscribe(([before, after]) => {
-    if (before > after) {
+    filter(([before, after]) => (before - after) > scrollStep),
+    map(([before, after]) => before > after),
+).subscribe((e) => {
+    if (e) {
         header.classList.add('active-menu');
     } else {
         header.classList.remove('active-menu');
