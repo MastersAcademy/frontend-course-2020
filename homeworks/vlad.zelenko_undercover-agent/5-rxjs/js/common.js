3bdd39e317992894aa19c905fd5def4674c00720
VladZelenko <Vlad6555z@gmail.com>
2020-11-22 11:20:48 +0200

    added filter events > 50

--- homeworks/vlad.zelenko_undercover-agent/5-rxjs/js/common.js
+++ homeworks/vlad.zelenko_undercover-agent/5-rxjs/js/common.js
@@ -1,18 +1,18 @@
 const { fromEvent } = window.rxjs;
 const { map, pairwise, filter } = window.rxjs.operators;
-const header = document.querySelector('[data-header]');
+const headerElement = document.querySelector('[data-header]');
 const scrollEvent = fromEvent(window, 'scroll');
 const scrollStep = 50;
 
 scrollEvent.pipe(
     map(() => window.pageYOffset),
     pairwise(),
-    filter(([before, after]) => (before - after) > scrollStep),
-    map(([before, after]) => before > after),
+    filter(([before, after]) => (after - before) > scrollStep || (before - after) > scrollStep),
+    map(([before, after]) => before < after),
 ).subscribe((e) => {
     if (e) {
-        header.classList.add('active-menu');
+        headerElement.classList.add('active-menu');
     } else {
-        header.classList.remove('active-menu');
+        headerElement.classList.remove('active-menu');
     }
 });
