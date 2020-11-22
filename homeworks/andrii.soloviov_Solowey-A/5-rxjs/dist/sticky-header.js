723facfcd677de34fef70a7b2ca91969e7fd416d
Solowey-A <Solowey2386@gmail.com>
2020-11-22 21:25:34 +0200

    fix js & css

--- homeworks/andrii.soloviov_Solowey-A/5-rxjs/dist/sticky-header.js
+++ homeworks/andrii.soloviov_Solowey-A/5-rxjs/dist/sticky-header.js
@@ -1,16 +1,16 @@
 const { fromEvent } = window.rxjs;
 const {
-    debounceTime, map, pairwise, filter, distinctUntilChanged,
+    throttleTime, map, pairwise, filter, distinctUntilChanged,
 } = window.rxjs.operators;
 
 const documentEl = document.querySelector('[data-sticky-header]');
 fromEvent(window, 'scroll')
     .pipe(
         map(() => window.pageYOffset),
-        debounceTime(100),
+        throttleTime(100),
         pairwise(),
-        filter(([prevPos, curPos]) => Math.abs(prevPos - curPos) > 50),
-        map(([prevPos, curPos]) => prevPos < curPos),
+        filter(([prevPosition, curPosition]) => Math.abs(prevPosition - curPosition) > 50),
+        map(([prevPosition, curPosition]) => prevPosition < curPosition),
         distinctUntilChanged(),
     )
-    .subscribe((result) => documentEl.classList.toggle('sticky-header', result));
+    .subscribe((value) => documentEl.classList.toggle('hide-header', value));
