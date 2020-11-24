a89398401d2f1b90d868b548b4d37a82a4f87eda
VladZelenko <Vlad6555z@gmail.com>
2020-11-24 19:02:21 +0200

    rename class hiden element and created short method filters events

--- homeworks/vlad.zelenko_undercover-agent/5-rxjs/js/common.js
+++ homeworks/vlad.zelenko_undercover-agent/5-rxjs/js/common.js
@@ -2,17 +2,17 @@ const { fromEvent } = window.rxjs;
 const { map, pairwise, filter } = window.rxjs.operators;
 const headerElement = document.querySelector('[data-header]');
 const scrollEvent = fromEvent(window, 'scroll');
-const step = 50;
+const scrollStep = 50;
 
 scrollEvent.pipe(
     map(() => window.pageYOffset),
     pairwise(),
-    filter(([before, after]) => Math.abs((after - before) > step || (before - after) > step)),
+    filter(([before, after]) => Math.abs(before - after) > scrollStep),
     map(([before, after]) => before < after),
 ).subscribe((activeStatus) => {
     if (activeStatus) {
-        headerElement.classList.add('hide-header');
+        headerElement.classList.add('header--hidden');
     } else {
-        headerElement.classList.remove('hide-header');
+        headerElement.classList.remove('header--hidden');
     }
 });
