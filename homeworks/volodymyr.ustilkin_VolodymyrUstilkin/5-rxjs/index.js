d1aec6c888388b7fd1ef8099e6aa1497b207ff8d
Volodymyr Ustilkin <volodymyr.ustilkin@gmail.com>
2020-11-24 23:50:04 +0200

    hw5 fix task 1*

--- homeworks/volodymyr.ustilkin_VolodymyrUstilkin/5-rxjs/index.js
+++ homeworks/volodymyr.ustilkin_VolodymyrUstilkin/5-rxjs/index.js
@@ -18,7 +18,12 @@
     const contentEl = document.querySelector('[data-main-container]');
     const buttonEl = document.querySelector('[data-button]');
 
-    function setHeaderButton() {
+    function setHeaderLogoButton() {
+        headerEl.innerHTML = '<div class="header-inner">LOGO</div>'
+            + '<button class="buy-button header-inner" type="button">BUY NOW</button>';
+    }
+
+    function setHeaderDiscountButton() {
         headerEl.innerHTML = '<div class="header-inner">Get an amazing<br>discount</div>'
             + '<button class="buy-button header-inner" type="button">BUY NOW</button>';
     }
@@ -46,8 +51,18 @@
             throttleTime(100),
             map((e) => e.currentTarget.scrollTop),
             pairwise(),
-            filter((e) => ((e[0] - e[1] > 50 && e[1] > 1) || (buttonEl.offsetTop < e[1]))),
-            tap((e) => (buttonEl.offsetTop < e[1] ? setHeaderButton() : setHeaderLogo())),
+            filter((e) => ((e[0] - e[1] > 50 && e[1] > 1) && (buttonEl.offsetTop > e[1]))),
+            tap(() => (setHeaderLogo())),
+        )
+        .subscribe(() => showHeader());
+
+    fromEvent(contentEl, 'scroll') // scroll up (show header *)
+        .pipe(
+            throttleTime(100),
+            map((e) => e.currentTarget.scrollTop),
+            pairwise(),
+            filter((e) => ((Math.abs(e[0] - e[1]) > 50) && (buttonEl.offsetTop < e[1]))),
+            tap((e) => (e[0] < e[1] ? setHeaderDiscountButton() : setHeaderLogoButton())),
         )
         .subscribe(() => showHeader());
 
