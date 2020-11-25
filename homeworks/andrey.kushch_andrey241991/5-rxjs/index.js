7c6d9d64d3183bf2a38d4bec61ddfde7380c3698
Andrey Kushch <droni2442@gmail.com>
2020-11-25 19:36:26 +0200

    Fix task

--- homeworks/andrey.kushch_andrey241991/5-rxjs/index.js
+++ homeworks/andrey.kushch_andrey241991/5-rxjs/index.js
@@ -1,31 +1,32 @@
 const { Observable, fromEvent } = window.rxjs;
+const { throttleTime, map, pairwise, take, filter } = window.rxjs.operators;
 
 const headerElem = document.querySelector('[data-header]');
 
-let scrollPossition = 0;
-let currentScrollPossition = 0;
-fromEvent(window, 'scroll').subscribe(() => {
-    currentScrollPossition = window.scrollY;
-});
+fromEvent(window, 'scroll')
+    .pipe(
+        throttleTime(200),
+        map((ev) => ev.path[1].scrollY),
+        pairwise(),
+        map((ev) => {
+            const prevY = ev[0];
+            const nextY = ev[1];
+            if (prevY - nextY <= -50) {
+                return false;
+            }
+            if (prevY - nextY >= 50) {
+                return true;
+            }
+        })
+    )
+    .subscribe((show) => {
+        if (show) {
+            headerElem.classList.add('header__active');
+            headerElem.classList.remove('header__not-active');
+        }
 
-const stream = new Observable((observer) => {
-    setInterval(() => {
-        observer.next(currentScrollPossition);
-    }, 400);
-});
-
-function toggleHeader(currentSP) {
-    if (scrollPossition - currentSP <= -50) {
-        scrollPossition = currentSP;
-        headerElem.classList.add('header__not-active');
-        headerElem.classList.remove('header__active');
-    }
-
-    if (scrollPossition - currentSP >= 50) {
-        scrollPossition = currentSP;
-        headerElem.classList.add('header__active');
-        headerElem.classList.remove('header__not-active');
-    }
-}
-
-stream.subscribe((val) => toggleHeader(val));
+        if (show === false) {
+            headerElem.classList.add('header__not-active');
+            headerElem.classList.remove('header__active');
+        }
+    });
