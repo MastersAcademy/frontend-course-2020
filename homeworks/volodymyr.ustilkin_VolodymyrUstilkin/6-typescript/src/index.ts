a21c84bc0bb91cc97c9ff34f992d556041a1bd84
Volodymyr Ustilkin <volodymyr.ustilkin@gmail.com>
2020-11-26 09:26:29 +0200

    hw6 init

---
+++ homeworks/volodymyr.ustilkin_VolodymyrUstilkin/6-typescript/src/index.ts
@@ -0,0 +1,48 @@
+const scoreElement = document.querySelector('[data-score]') as HTMLHeadingElement;
+const cubeElement = document.querySelector('[data-cube]') as HTMLDivElement;
+const cubeScoreElement = document.querySelector('[data-cube-score]') as HTMLDivElement;
+const keyElement = document.querySelector('[data-key]') as HTMLDivElement;
+const progressBarElement = document.querySelector('[data-progress-bar]') as HTMLDivElement;
+
+class Game {
+    private score: number = 100;
+    private currentKey: string = '';
+    private interval: number = 2000;
+
+    constructor(
+        private scoreElement: HTMLHeadingElement,
+        private cubeScoreElement: HTMLDivElement,
+        private keyElement: HTMLDivElement,
+        private cubeElement?: HTMLDivElement,
+        private progressBarElement?: HTMLDivElement,
+    ) {
+    }
+
+    start() {
+        // code...
+    }
+
+    private startKeysInterval() {
+        // code...
+    }
+
+    private setScore(score: number) {
+        // code...
+    }
+
+    private setKey(key: string) {
+        // code...
+    }
+
+    private addScore(score: number) {
+        // code...
+    }
+
+    private subscribeOnKeyPress() {
+        // code...
+    }
+}
+
+const game = new Game(scoreElement, cubeScoreElement, keyElement, cubeElement, progressBarElement);
+
+game.start();
---
+++ homeworks/volodymyr.ustilkin_VolodymyrUstilkin/6-typescript/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
+    plugins: [
+        new CleanWebpackPlugin({ cleanStaleWebpackAssets: true }),
+        new HtmlWebpackPlugin({
+            title: '6-Typescript'
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
