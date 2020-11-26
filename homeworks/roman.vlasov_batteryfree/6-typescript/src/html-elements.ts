03c056e1a005490b67e981a332b0bd6c5c2f4001
batteryfree <battery.free@gmail.com>
2020-11-26 19:57:31 +0200

    added hw-6

---
+++ homeworks/roman.vlasov_batteryfree/6-typescript/src/html-elements.ts
@@ -0,0 +1,7 @@
+export interface ElementsHTML {
+    scoreEl: HTMLHeadingElement;
+    cubeScoreEl: HTMLHeadingElement;
+    cubeEl: HTMLHeadingElement;
+    keyEl: HTMLHeadingElement;
+    progressBarEl: HTMLHeadingElement;
+}
