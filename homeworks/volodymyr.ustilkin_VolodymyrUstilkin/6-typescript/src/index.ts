a59d51c4fac7212e7b7af410c19e5619edcfba97
Volodymyr Ustilkin <volodymyr.ustilkin@gmail.com>
2020-11-26 17:19:06 +0200

    hw6 base logic

--- homeworks/volodymyr.ustilkin_VolodymyrUstilkin/6-typescript/src/index.ts
+++ homeworks/volodymyr.ustilkin_VolodymyrUstilkin/6-typescript/src/index.ts
@@ -1,5 +1,3 @@
-//import css from "index.css";
-
 const scoreElement = document.querySelector('[data-score]') as HTMLHeadingElement;
 const cubeElement = document.querySelector('[data-cube]') as HTMLDivElement;
 const cubeScoreElement = document.querySelector('[data-cube-score]') as HTMLDivElement;
@@ -10,16 +8,16 @@ class Game {
     private score: number = 100;
     private currentKey: string = '';
     private interval: number = 2000;
-    private intervalId : number;
-    private intervalEventDelay: number = 10;
+    private intervalId : number = 0;
+    private intervalEventDelay: number = 100;
     private currentEventTime: number = 0;
 
     constructor(
         private scoreElement: HTMLHeadingElement,
         private cubeScoreElement: HTMLDivElement,
         private keyElement: HTMLDivElement,
-        private cubeElement?: HTMLDivElement,
-        private progressBarElement?: HTMLDivElement,
+        private cubeElement: HTMLDivElement,
+        private progressBarElement: HTMLDivElement,
     ) {
     }
 
@@ -27,35 +25,67 @@ class Game {
         this.subscribeOnKeyPress();
         this.startKeysInterval();
         this.generateKey();
-        console.log('started');
     }
 
     private startKeysInterval() {
-        this.intervalId = window.setInterval(this.intervalEvent, this.currentEventTime);
+        this.intervalId = window.setInterval(this.intervalEvent, this.intervalEventDelay, this);
     }
 
-    private intervalEvent() {
-        this.currentEventTime += this.currentEventTime;
-        if (this.currentEventTime > this.interval) {
-            this.addScore(-20);
+    private intervalEvent(current : Game) {
+        current.currentEventTime += current.intervalEventDelay;
+        current.changeProgressBar();
+        if (current.currentEventTime > current.interval) {
+            current.addScore(current.generateNumber(-10, -15));
+            current.resetEvent();
+            console.log('time failed');
         }
     }
 
+    private changeProgressBar() {
+        let maxSize : number = 200 / this.interval;
+        let step = this.interval / maxSize;
+        let size : number = 200;
+        this.progressBarElement.style.width = String(100 - this.currentEventTime/this.interval*100) +'%';
+        console.log(this.currentEventTime/this.interval);
+
+    }
+
     private setScore(score: number) {
-        scoreElement.innerHTML = score.toString();
+        this.scoreElement.innerHTML = score >= 0 ? '+' + score.toString() : score.toString();
+        if (score > 200) {
+            alert('victory');
+            this.restartGame();
+        }
+        if (score < 0) {
+            alert('loss');
+            this.restartGame();
+        }
+    }
+
+    private restartGame() {
+        this.scoreElement.innerHTML = this.score.toString();
+        this.cubeScoreElement.innerHTML = '';
+        window.clearInterval(this.intervalId);
     }
 
     private setKey(key: string) {
         if (key.toUpperCase() === keyElement.innerHTML) {
-            this.addScore(10);
+            let score: number = this.generateNumber(5, 10);
+            this.addScore(score);
         } else {
-            this.addScore(-25);
+            this.addScore(this.generateNumber(-20, -25));;
         }
+        this.resetEvent();
+    }
+
+    private resetEvent() {
+        this.currentEventTime = 0;
         this.generateKey();
     }
 
     private addScore(score: number) {
-        let currentScore: number = Number.parseInt(scoreElement.innerHTML);
+        let currentScore: number = Number.parseInt(this.scoreElement.innerHTML);
+        this.cubeScoreElement.innerHTML = score.toString();
         this.setScore(currentScore + score);
 
     }
@@ -67,8 +97,12 @@ class Game {
     private generateKey(): void {
         let aCode :number = 'A'.charCodeAt(0);
         let zCode :number = 'Z'.charCodeAt(0);
-        let generatedCode: number = Math.floor(Math.random() * Math.floor(zCode - aCode)) + aCode;
+        let generatedCode: number = this.generateNumber(aCode, zCode);
         keyElement.innerHTML = String.fromCharCode(generatedCode);
+    }
+
+    private generateNumber (min: number, max : number) : number {
+        return Math.floor(Math.random() * Math.floor(max - min)) + min;
     }
 }
 
