1b8842494f8e356772012ff417ce1bd97065ee1e
Anna Vrana <anna.o.vrana@gmail.com>
2020-11-29 12:21:46 +0200

    add interval

--- homeworks/anna.vrana_Anna1o1Vrana/6-typescript/src/scripts/index.ts
+++ homeworks/anna.vrana_Anna1o1Vrana/6-typescript/src/scripts/index.ts
@@ -1,9 +1,8 @@
 import '../styles/style.css';
 import '../styles/normalize.css';
 
+const containerElement = document.querySelector('[data-container]') as HTMLDivElement;
 const scoreElement = document.querySelector('[data-score]') as HTMLHeadingElement;
-const cubeElement = document.querySelector('[data-cube]') as HTMLDivElement;
-const cubeScoreElement = document.querySelector('[data-cube-score]') as HTMLDivElement;
 const keyElement = document.querySelector('[data-key]') as HTMLDivElement;
 const progressBarElement = document.querySelector('[data-progress-bar]') as HTMLDivElement;
 const startBtnElement = document.querySelector('[data-button]') as HTMLButtonElement;
@@ -12,14 +11,14 @@ const letters: string[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'
 class Game {
     private score: number = 100;
     private currentKey: string = '';
+    private pressedKey: string = ';'
     private interval: number = 2000;
     private isGameStarted: boolean = false;
 
     constructor(
+        private containerElement: HTMLDivElement,
         private scoreElement: HTMLHeadingElement,
-        private cubeScoreElement: HTMLDivElement,
         private keyElement: HTMLDivElement,
-        private cubeElement?: HTMLDivElement,
         private progressBarElement?: HTMLDivElement,
     ) {
     }
@@ -28,13 +27,39 @@ class Game {
     start() {
         this.isGameStarted = true;
         this.scoreElement.innerHTML = String(this.score);
-        this.getLetters();
-        this.subscribeOnKeyPress();
+                this.getLetters();
+                this.subscribeOnKeyPress();
+                this.startKeysInterval();
+                this.endGame();
+    }
+
+    private startKeysInterval() {
+        let counterProgress: number = 0;
+        const progress = setInterval(() => {
+            counterProgress++;
+            this.progressBarElement.style.width = `${Number(this.progressBarElement.style.width.slice(0, -1)) + 1}%`
+            if (counterProgress === 100) {
+                this.progressBarElement.style.width = '0%';
+            }
+                if (this.pressedKey === this.currentKey) {
+                    this.progressBarElement.style.width = '0%';
+                }
+        }, this.interval / 100);
+
+    }
+    private setScore(score: number) {
+        // code...
+    }
+
+    private setKey(key: string) {
+        // code...
     }
 
     private getLetters(): void {
-        this.currentKey = letters[Math.floor(Math.random() * letters.length)];
-        keyElement.innerHTML = this.currentKey;
+        setInterval(() => {
+            this.currentKey = letters[Math.floor(Math.random() * letters.length)];
+            keyElement.innerHTML = this.currentKey;
+        }, this.interval)
     }
 
     private getRandom(min: number, max: number) {
@@ -42,6 +67,7 @@ class Game {
     }
 
     private subscribeOnKeyPress() {
+        setInterval(() => {
         document.addEventListener('keypress', (event: KeyboardEvent) => {
             const keyName = event.key;
             if (keyName.toLowerCase() === this.currentKey.toLowerCase()) {
@@ -52,22 +78,27 @@ class Game {
                 this.scoreElement.innerHTML = String(this.score -= this.getRandom(10, 15));
             }
         });
+        }, this.interval)
     }
 
     private endGame() {
+        document.addEventListener('keypress', (event: KeyboardEvent) => {
         if (this.score >= 200) {
-            alert('Congratulations!!! You awesome!!!');
+            containerElement.innerHTML = 'Congratulations!!! You awesome!!! ( ͡❛ ͜ʖ ͡❛)✌';
             this.isGameStarted = false;
         } else if (this.score <= 0) {
-            alert('You lose XD');
+            containerElement.innerHTML ='You lose ( ͡❛ ͜ʖ ͡❛)';
             this.isGameStarted = false;
         } else {
             this.isGameStarted = true;
         }
+    })
     }
 }
 
-const game = new Game(scoreElement, cubeScoreElement, keyElement, cubeElement, progressBarElement);
+const game = new Game(containerElement, scoreElement, keyElement, progressBarElement);
+
+// startBtnElement.addEventListener('click', game.start();
 
 game.start();
 
