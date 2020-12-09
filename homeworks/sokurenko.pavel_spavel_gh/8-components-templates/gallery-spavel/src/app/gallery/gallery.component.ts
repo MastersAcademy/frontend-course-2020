e110feb0b1e8cd380bddffea7f27d1cc31224473
spavel-gh <spavel.mail@gmail.com>
2020-12-09 15:52:38 +0200

    gallery part prototype

--- homeworks/sokurenko.pavel_spavel_gh/8-components-templates/gallery-spavel/src/app/gallery/gallery.component.ts
+++ homeworks/sokurenko.pavel_spavel_gh/8-components-templates/gallery-spavel/src/app/gallery/gallery.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { IMAGES } from '../../assets/data';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.scss']
+  styleUrls: ['./gallery.component.scss'],
 })
 export class GalleryComponent implements OnInit {
+  images = IMAGES;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  selectImageId(event: Event): void {
+    console.log((event.target as Element).id);
   }
-
 }
