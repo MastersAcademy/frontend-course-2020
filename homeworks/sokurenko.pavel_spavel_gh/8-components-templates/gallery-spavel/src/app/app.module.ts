72503c244bbce07aeb48f459009fd879d4524767
spavel-gh <spavel.mail@gmail.com>
2020-12-09 12:18:15 +0200

    create components

--- homeworks/sokurenko.pavel_spavel_gh/8-components-templates/gallery-spavel/src/app/app.module.ts
+++ homeworks/sokurenko.pavel_spavel_gh/8-components-templates/gallery-spavel/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ImageComponent } from './image/image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryComponent,
+    ImageComponent
   ],
   imports: [
     BrowserModule,
