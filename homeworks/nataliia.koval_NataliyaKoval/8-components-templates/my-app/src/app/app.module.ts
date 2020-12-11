db0745c882eda44803ec1d54603d2187b6866583
Nataliia Koval <hakunata@gmail.com>
2020-12-11 12:12:35 +0200

    added interfaces and spinner

--- homeworks/nataliia.koval_NataliyaKoval/8-components-templates/my-app/src/app/app.module.ts
+++ homeworks/nataliia.koval_NataliyaKoval/8-components-templates/my-app/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GalleryComponent } from './gallery/gallery.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { GalleryComponent } from './gallery/gallery.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
