a3e48bfc9888204e90063e55043b34f75e05efbc
Anna Vrana <anna.o.vrana@gmail.com>
2020-12-16 19:42:19 +0200

    change class-names, add image title, delete optional change

--- homeworks/anna.vrana_Anna1o1Vrana/8-components-templates/my-app-gallery/src/app/all-images/all-images.component.ts
+++ homeworks/anna.vrana_Anna1o1Vrana/8-components-templates/my-app-gallery/src/app/all-images/all-images.component.ts
@@ -8,14 +8,18 @@ import {IMAGES, Img} from "../constants/data";
 })
 
 export class AllImagesComponent implements OnInit {
-
-  public images: Img[] = IMAGES;
-  public active: Img | undefined;
-  onActiveImg(activeImg: Img): any {
-    this.active = this.active?.id === activeImg.id ? undefined : activeImg;
-  }
-
   ngOnInit(): void {
 
   }
+  public images: Img[] = IMAGES;
+  public active: Img | undefined;
+  onActiveImg(activeImg: Img): any {
+    if (this.active?.id === activeImg.id){
+      this.active = undefined;
+    } else {
+      this.active = activeImg;
+    }
+  }
+
+
 }
