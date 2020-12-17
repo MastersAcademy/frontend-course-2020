036701235cc160bf7ddef9073734ed41f4ea21fa
Anna Vrana <anna.o.vrana@gmail.com>
2020-12-17 23:06:22 +0200

    create loader, delete ngOnInit

--- homeworks/anna.vrana_Anna1o1Vrana/8-components-templates/my-app-gallery/src/app/all-images/all-images.component.ts
+++ homeworks/anna.vrana_Anna1o1Vrana/8-components-templates/my-app-gallery/src/app/all-images/all-images.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit} from '@angular/core';
-import {IMAGES, Img} from "../data/data";
+import {Component} from '@angular/core';
+import {Img} from "../data/Img";
+import {IMAGES} from "../data/data";
 
 @Component({
   selector: 'app-all-images',
@@ -7,17 +8,21 @@ import {IMAGES, Img} from "../data/data";
   styleUrls: ['./all-images.component.css']
 })
 
-export class AllImagesComponent implements OnInit {
-  ngOnInit(): void {
+export class AllImagesComponent {
 
-  }
   public images: Img[] = IMAGES;
   public active?: Img;
+  public loading: boolean = true;
+
   onActiveImg(activeImg: Img): void {
     if (this.active?.id === activeImg.id){
       this.active = undefined;
     } else {
       this.active = activeImg;
     }
+  }
+
+  hideLoader($event : any)  : void {
+    this.loading= false;
   }
 }
