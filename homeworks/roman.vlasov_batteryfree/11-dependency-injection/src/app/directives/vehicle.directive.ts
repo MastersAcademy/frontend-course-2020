c236b67a77c14c15d919bfd884eacb77c3a73c97
batteryfree <battery.free@gmail.com>
2020-12-24 17:48:04 +0200

    added *

--- homeworks/roman.vlasov_batteryfree/11-dependency-injection/src/app/directives/vehicle.directive.ts
+++ homeworks/roman.vlasov_batteryfree/11-dependency-injection/src/app/directives/vehicle.directive.ts
@@ -1,19 +1,37 @@
-import { AfterViewInit, Directive, Input, ElementRef } from '@angular/core';
+import {
+    AfterViewInit,
+    Directive,
+    Input,
+    ElementRef,
+    Renderer2
+} from '@angular/core';
+
 const pattern = /(^https)/;
+const url = 'https://www.google.com/search?q=';
 
 @Directive({
     selector: '[appVehicle]'
 })
+
 export class VehicleDirective implements AfterViewInit {
 
     @Input() appVehicle: string = '';
+    @Input() href: string | null = null;
 
-    constructor(private elementRef: ElementRef) {
+
+    constructor(private renderer: Renderer2, private elementRef: ElementRef) {
+        this.renderer.setAttribute(this.elementRef.nativeElement, 'href', '');
     }
 
     ngAfterViewInit(): void {
         if (!pattern.test(this.appVehicle)) {
-            this.elementRef.nativeElement.style.color = '#F00';
+            this.renderer.setStyle(this.elementRef.nativeElement, 'color', '#F00');
+        }
+
+        if (this.href !== null) {
+            this.renderer.setAttribute(this.elementRef.nativeElement, 'href',
+                `${url}${this.href.replace(/ /g, "+")}`);
+            this.renderer.setAttribute(this.elementRef.nativeElement, 'target', '_blank');
         }
     }
 }
