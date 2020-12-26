30d219c073e300703a98adaf1d54cc58c940e42a
rostyslav <ronyahavuka@gmail.com>
2020-12-26 14:18:36 +0200

    Changed

--- homeworks/rostyslav.havuka_RostyslavHavuka/11-dependency-injection/vehicle/src/app/directives/http-car.directive.ts
+++ homeworks/rostyslav.havuka_RostyslavHavuka/11-dependency-injection/vehicle/src/app/directives/http-car.directive.ts
@@ -5,7 +5,7 @@ import { AfterViewInit, Directive, ElementRef, Input } from '@angular/core';
 })
 
 export class ChangeHttp implements AfterViewInit {
-  @Input() http: HTMLElement;
+  @Input() http;
   constructor(private element: ElementRef) {}
 
   ngAfterViewInit(): void {
