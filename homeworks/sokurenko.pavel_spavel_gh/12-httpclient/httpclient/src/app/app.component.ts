11d3de5e3bb0726526ec7a8e0352ef5e38730af2
spavel-gh <spavel.mail@gmail.com>
2021-01-02 21:57:43 +0200

    add http

--- homeworks/sokurenko.pavel_spavel_gh/12-httpclient/httpclient/src/app/app.component.ts
+++ homeworks/sokurenko.pavel_spavel_gh/12-httpclient/httpclient/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { UserService } from './services/user.service';
+import { User } from './models/user.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'httpclient';
+
+  userData: User[] = [];
+  private subscription: Subscription = new Subscription();
+
+  constructor(private userService: UserService) {}
+
+
+
+  ngOnInit(): void {
+    this.subscription.add(this.userService.getUsers().subscribe((users: User[]) => {
+      this.userData = users;
+      console.log('ok');
+    }));
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
---
+++ homeworks/sokurenko.pavel_spavel_gh/12-httpclient/httpclient/src/app/services/user.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { User } from '../models/user.model';
+import { Page } from '../models/page.model';
+
+@Injectable({providedIn: 'root'})
+
+export class UserService {
+  ROOT_URL = 'https://reqres.in/api/users';
+  options = {
+    params: { page: '2' },
+    headers: { Authorization: 'i2ojjfsoiejfs9' }
+  };
+
+  constructor(private http: HttpClient) { }
+  // constructor() { }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<Page>(this.ROOT_URL, this.options).pipe(
+      map(({ data }) => data)
+    );
+  }
+
+
+
+}
