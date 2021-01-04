15e31d6b855a8dc6755a7006bc33653be35599a0
Anna Vrana <anna.o.vrana@gmail.com>
2021-01-04 09:11:51 +0200

    deleted excessive app

--- homeworks/anna.vrana_Anna1o1Vrana/8-components-templates/galleryHw/src/app/img-list/img-list.component.ts
+++ homeworks/anna.vrana_Anna1o1Vrana/8-components-templates/galleryHw/src/app/img-list/img-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { IMAGES} from '../data/data';
 import {Img} from '../data/Img';
 
@@ -7,14 +7,10 @@ import {Img} from '../data/Img';
   templateUrl: './img-list.component.html',
   styleUrls: ['./img-list.component.css']
 })
-export class ImgListComponent implements OnInit {
+export class ImgListComponent {
   public selectedImg: Img;
   public images: Img[] = IMAGES;
   isSelect: boolean;
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
   onSelect(img: Img): void {
     this.selectedImg = img;
