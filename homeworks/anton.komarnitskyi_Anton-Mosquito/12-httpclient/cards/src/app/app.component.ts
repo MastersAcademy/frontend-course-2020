0a0d8f3788ffb445656fc0016b349788cb288e07
Anton <Mosquito>
2021-01-05 12:05:20 +0200

    first

---
+++ homeworks/anton.komarnitskyi_Anton-Mosquito/12-httpclient/cards/src/app/app.component.ts
@@ -0,0 +1,76 @@
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
+import { fromEvent, range, Subject, Subscription} from 'rxjs';
+import { User } from './models/user.models';
+import { Page } from './models/page.model';
+import { UserService } from './services/user.service';
+import { LoaderService } from './services/loading.service';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChildren('buttons') buttons! : QueryList<ElementRef>;
+
+  public loadingPagination: boolean = false;
+  public page: number = 1;
+  public quantityCardsOfPage : number = 2;
+  public quantityButton : number[] = [];
+  public userData: User[] = [];
+  private subscription: Subscription = new Subscription();
+  public isLoadingContent: Subject<boolean> = this.loaderService.isLoadingContent;
+
+  constructor(private userService: UserService, private loaderService: LoaderService) {}
+
+  private getUsers() :void{
+    this.subscription.add(this.userService.getUsers(this.quantityCardsOfPage,this.page).subscribe((users: User[]) => {
+      this.userData = users;
+    }));
+  }
+
+  public changePage(event:MouseEvent) :void {
+    switch ((event.target as HTMLElement).dataset.page) {
+      case 'prev':
+        this.page -= 1
+        this.getUsers();
+      break;
+      case 'next':
+        this.page += 1;
+        this.getUsers();
+      break;
+    }
+  }
+
+  ngOnInit() :void{
+    this.getUsers();
+
+    this.subscription.add(this.userService.getQuantityOfUsers().subscribe((page: Page) => {
+      const count = Math.ceil(page.total / this.quantityCardsOfPage);
+      range(1,count).pipe(
+        map(i => this.quantityButton.push(i)))
+        .subscribe();
+        this.loadingPagination = true;
+      }));
+  }
+
+  ngAfterViewInit() :void {
+    this.subscription.add(this.buttons.changes.subscribe((element)=>{
+      element.toArray().forEach((element: ElementRef) => {
+        fromEvent<Event>(element.nativeElement, "click")
+        .subscribe((event)=> {
+
+          const innerData = (event.target as HTMLElement).innerText;
+          this.page = Number(innerData);
+          this.getUsers();
+
+        });
+      });
+    }));
+  }
+
+  ngOnDestroy() :void{
+    this.subscription.unsubscribe();
+  }
+}
---
+++ homeworks/anton.komarnitskyi_Anton-Mosquito/12-httpclient/cards/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { MyInterceptor } from './interceptor/interceptor';
+import { LoaderComponent } from './loader/loader.component';
+import { LoaderService } from './services/loading.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: MyInterceptor, multi: true },
+    LoaderService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
