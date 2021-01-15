0389aa9a320335aaf496fb319b23fa4c325b41db
Nataliia Koval <hakunata@gmail.com>
2021-01-15 17:08:32 +0200

    save encoded data into a localstorage, style

--- homeworks/nataliia.koval_NataliyaKoval/13-ng-forms/my-app/src/app/app.component.ts
+++ homeworks/nataliia.koval_NataliyaKoval/13-ng-forms/my-app/src/app/app.component.ts
@@ -11,17 +11,14 @@ export class AppComponent implements OnInit{
   form!: FormGroup;
   
   ngOnInit() {
-    this.form = new FormGroup({
+
+    this.form = new FormGroup ({
       email: new FormControl('', [Validators.email, Validators.required]),
-      password: new FormControl(null, [Validators.minLength(6), Validators.required])
+      password: new FormControl(null, [Validators.minLength(6), Validators.required]),
+      rememberMe: new FormControl(false)
     });
-  }
 
-  submit(): void {
-    if (this.form.valid) {
-      alert(`You email: ${this.form.value.email}, password: ${this.form.value.password}`);
-
-    }
+    this.checkLocalStorage();
   }
 
   get email() { 
@@ -32,4 +29,45 @@ export class AppComponent implements OnInit{
     return this.form.get('password')!; 
   }
 
+  get rememberMe() { 
+    return this.form.get('rememberMe')!; 
+  }
+
+  checkLocalStorage() {
+    if (localStorage.getItem('inputsData')) {
+      const data = this.decodInputsData();
+      this.form.setValue({
+        email: data.email,
+        password: data.password,
+        rememberMe: true
+      })    
+    }
+  }
+
+  submit(): void {
+    if (this.form.valid) {
+      alert(`You email: ${this.email.value}, password: ${this.password.value}`);
+      
+      if (this.rememberMe.value) {
+        this.encodInputsData();
+        this.saveToLocalStorage();
+      }
+    }
+  }
+
+  encodInputsData() {
+    const encodedInputsData: string = window.btoa(JSON.stringify(this.form.value));
+    return encodedInputsData;
+  }
+
+  saveToLocalStorage(): void {
+    const encodedInputsData = this.encodInputsData();
+    localStorage.setItem('inputsData', encodedInputsData);
+  }
+
+  decodInputsData() {
+    const decodedInputsData: string = atob(<string>localStorage.getItem('inputsData'));
+    const inputsData = JSON.parse(decodedInputsData);
+    return inputsData;
+  }
 }
