d81a068264544921f727b28e7816013d72a700e5
spavel-gh <spavel.mail@gmail.com>
2021-01-17 16:01:10 +0200

    add validation and save save data in local storage

--- homeworks/sokurenko.pavel_spavel_gh/13-ng-forms/ng-forms/src/app/app.component.ts
+++ homeworks/sokurenko.pavel_spavel_gh/13-ng-forms/ng-forms/src/app/app.component.ts
@@ -1,27 +1,34 @@
-import { Component } from '@angular/core';
-import {  FormGroup, FormControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {  FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ng-forms';
+export class AppComponent implements OnInit {
 
   authForm = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
     remember: new FormControl(''),
   });
 
 
-
+  ngOnInit(): void {
+    if ((localStorage.getItem('email') !== null) && (localStorage.getItem('password') !== null)) {
+      this.authForm.setValue({
+        email: atob(localStorage.getItem('email')),
+        password: atob(localStorage.getItem('password')),
+        remember: true
+      });
+    }
+  }
 
 
   public onSubmit(): void {
-    // console.warn(this.authForm.value);
     this.printFormFieldsAlert();
+    this.updateLoacalStorage(this.authForm.value.remember);
   }
 
   public printFormFieldsAlert(): void {
@@ -31,5 +38,17 @@ export class AppComponent {
       '\npassword:  ' + this.authForm.value.password +
       '\nremember:  ' + this.authForm.value.remember
       );
+    localStorage.setItem('email', btoa(this.authForm.value.email.toString()));
+    localStorage.setItem('password', btoa(this.authForm.value.password.toString()));
+  }
+
+  public updateLoacalStorage(remember: boolean): void {
+    if (remember === true) {
+      localStorage.setItem('email', btoa(this.authForm.value.email.toString()));
+      localStorage.setItem('password', btoa(this.authForm.value.password.toString()));
+    } else {
+      localStorage.removeItem('email');
+      localStorage.removeItem('password');
+    }
   }
 }
