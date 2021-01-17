9e3687647ac699de3652a68caa8aa3032a9de62d
spavel-gh <spavel.mail@gmail.com>
2021-01-17 13:35:52 +0200

    form fields binding with ngModel

--- homeworks/sokurenko.pavel_spavel_gh/13-ng-forms/ng-forms/src/app/app.component.ts
+++ homeworks/sokurenko.pavel_spavel_gh/13-ng-forms/ng-forms/src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-forms';
+  text = '';
+
+  form  = {
+    email: '123',
+    password: '321',
+    remember: false
+  };
+
+  public submit(): void {
+    // console.log(a);
+    // console.log(b);
+    // alert('Warning');
+    this.printFormFieldsAlert();
+  }
+
+  public printFormFieldsAlert(): void {
+    alert(
+      '\nFORM FIELDS\n\n' +
+      'email:  ' + this.form.email.toString() +
+      '\npassword:  ' + this.form.password.toString() +
+      '\nremember:  ' + this.form.remember
+      );
+  }
 }
