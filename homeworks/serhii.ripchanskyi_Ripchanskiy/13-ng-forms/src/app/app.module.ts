6269d35bb139966af4cbf13cbe671a5623d7f70d
Sergey Ripchanskiy <sergeyripchanskiy@gmail.com>
2021-01-17 02:43:53 +0200

    login form

--- homeworks/serhii.ripchanskyi_Ripchanskiy/13-ng-forms/src/app/app.module.ts
+++ homeworks/serhii.ripchanskyi_Ripchanskiy/13-ng-forms/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
