b7870145e56c50133016316db8e6171ebdbdce5c
Volodymyr Ustilkin <volodymyr.ustilkin@gmail.com>
2021-01-20 15:08:57 +0200

    hw14 create structure

--- homeworks/volodymyr.ustilkin_VolodymyrUstilkin/14-modules-routing/modules-routing/src/app/app-routing.module.ts
+++ homeworks/volodymyr.ustilkin_VolodymyrUstilkin/14-modules-routing/modules-routing/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./modules/home/components/home/home.component";
+import {UserHistoryGuard} from "./guards/user-history.guard";
+import {AccountComponent} from "./modules/account/components/account/account.component";
+import {AccessoriesComponent} from "./modules/accessories/components/accessories/accessories.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent, canActivate: [UserHistoryGuard]},
+  {path: 'home', component: HomeComponent, canActivate: [UserHistoryGuard]},
+  {path: 'account', component: AccountComponent, canActivate: [UserHistoryGuard]},
+  {path: 'accessories', component: AccessoriesComponent, canActivate: [UserHistoryGuard]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- homeworks/volodymyr.ustilkin_VolodymyrUstilkin/14-modules-routing/modules-routing/src/app/app.module.ts
+++ homeworks/volodymyr.ustilkin_VolodymyrUstilkin/14-modules-routing/modules-routing/src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {services} from "./services";
+import {modules} from "./modules";
 
 @NgModule({
   declarations: [
@@ -11,11 +12,13 @@ import {services} from "./services";
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ...modules,
   ],
   providers: [
     services
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
