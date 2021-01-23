3cb223be0fcc3e7c55c0c035466dfa0a344f24ad
Anna Vrana <anna.o.vrana@gmail.com>
2021-01-24 01:09:42 +0200

    create project and modules:home, acount, accessories, shared

--- homeworks/anna.vrana_Anna1o1Vrana/14-modules-routing/modules-routing/src/app/app.module.ts
+++ homeworks/anna.vrana_Anna1o1Vrana/14-modules-routing/modules-routing/src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeModule} from "./modules/home/home.module";
+import {AccountModule} from "./modules/account/account.module";
+import {AccessoriesModule} from "./modules/accessories/accessories.module";
+import {SharingModule} from "./modules/sharing/sharing.module";
+// import { HomeComponent } from './home/home.component';
+// import { AccountComponent } from './account/account.component';
+// import { AccessoriesComponent } from './modules/accessories/components/accessories/accessories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // HomeComponent,
+    // AccountComponent,
+    // AccessoriesComponent,
+    // AccessoriesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule,
+    AccountModule,
+    AccessoriesModule,
+    SharingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
