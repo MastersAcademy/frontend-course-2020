5e4546fae5c44d10224628ced9c87ea33035245c
spavel-gh <spavel.mail@gmail.com>
2021-01-24 14:26:55 +0200

    linking modules

--- homeworks/sokurenko.pavel_spavel_gh/14-modules-routing/modules-routing/src/app/modules/shared/shared.module.ts
+++ homeworks/sokurenko.pavel_spavel_gh/14-modules-routing/modules-routing/src/app/modules/shared/shared.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { NavComponent } from './components/nav/nav.component';
 
 
-
 @NgModule({
-  declarations: [NavComponent],
+  declarations: [],
   imports: [
     CommonModule
+  ],
+  exports: [
+    NavComponent
   ]
 })
 export class SharedModule { }
