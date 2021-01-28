d1e5a6f197401bf782f755662a0c1f0de84ecffa
Volodymyr Ustilkin <volodymyr.ustilkin@gmail.com>
2021-01-28 23:28:27 +0200

    hw13 fix use enum on inputType

--- homeworks/volodymyr.ustilkin_VolodymyrUstilkin/13-ng-forms/ng-forms/src/app/custom-input/custom-input.component.ts
+++ homeworks/volodymyr.ustilkin_VolodymyrUstilkin/13-ng-forms/ng-forms/src/app/custom-input/custom-input.component.ts
@@ -1,6 +1,14 @@
 import {Component, forwardRef, Input, OnInit} from '@angular/core';
 import {ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR} from "@angular/forms";
 
+enum InputType {
+  text= 'text',
+  password = 'password',
+  number = 'number',
+  email = 'email',
+  hidden = 'hidden'
+}
+
 @Component({
   selector: 'app-custom-input',
   templateUrl: './custom-input.component.html',
@@ -15,11 +23,11 @@ export class CustomInputComponent implements ControlValueAccessor {
   readonly textInput = new FormControl('');
 
   @Input() labelName: string = '';
-  @Input() inputType: string;
+  @Input() inputType: InputType;
   @Input() inputTypeChangeButtonDisplay: boolean = false;
   @Input() labelErrorValue: string = '';
 
-  private previousInputType: string;
+  private previousInputType: InputType;
 
   constructor() {}
 
@@ -27,7 +35,7 @@ export class CustomInputComponent implements ControlValueAccessor {
   onChange = (value: any) => {};
 
   get isInputTypePassword(): boolean {
-    return this.inputType !== 'password';
+    return this.inputType !== InputType.password;
   }
 
   onInputChange(event: Event) {
@@ -55,7 +63,7 @@ export class CustomInputComponent implements ControlValueAccessor {
   changeTextInputType() {
     if (this.inputType !== 'password') {
       this.previousInputType = this.inputType;
-      this.inputType = 'password'
+      this.inputType = InputType.password
     } else {
       this.inputType = this.previousInputType;
     }
