21fe4cdba668250233c709c84fd762e159f44609
Anton <Mosquito>
2021-01-31 09:05:59 +0200

    fix

--- homeworks/anton.komarnitskyi_Anton-Mosquito/14-modules-routing/route/src/app/app.module.ts
+++ homeworks/anton.komarnitskyi_Anton-Mosquito/14-modules-routing/route/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { modules } from './content/modules';
-import { SharedModule } from './content/navigation/shared.module';
+import { SharedModule } from './shared/shared.module';
 import { services } from './services/index';
 
 @NgModule({
